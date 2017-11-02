7fbeb73a1cbe7d4dc6fc91bfd68d2bd3a9f0b80a
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2017-11-02 09:41:21 -0700

    completed hack hour

---
+++ challenges/draw-stairs.js
@@ -0,0 +1,31 @@
+/* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
+ The staircase must climb up from left to right. The last line should only consist of asterisks,
+ without any leading/trailing spaces.
+ 
+ For example:     
+ drawStairs(6) ->          
+     *
+    **
+   ***
+  ****
+ *****
+******
+
+*/
+
+function drawStairs(n) {
+    if (typeof n !== 'number') {return 'Input is not a number.'}
+    if (n < 1) {return 'Input is too small.'}
+    if (n > 100) {return 'Input is too large'}
+    let returnString = '';
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < n-i; j++) {
+            returnString += ' ';
+        } for (let k = i; k > 0; k--)  {
+            returnString += '*' 
+        } if (i < n) {returnString += '\n'}
+    } return returnString;
+}
+
+
+module.exports = drawStairs;
