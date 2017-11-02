7ea60ebc4b57754193a40b9529a1f6338b426a00
ryanlondon <ryel590@gmail.com>
2017-11-02 09:51:13 -0700

    for loops solution

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,28 @@
 */
 
 function drawStairs(n) {
+    if (typeof n !== 'number') return;
+    if (n < 1) n = 1;
+    if (n > 100) n = 100;
 
-}
+    let string = '';
+
+    function drawLine(lineNum) {
+        let line = '';
+        for (let pos = 0; pos < n; pos++) {
+            if (pos < (n - lineNum) - 1) line += ' ';
+            else line += '*';
+        }
+        if (lineNum < n - 1) line += '\n';
+        return line;
+    }
+
+    for (let i = 0; i < n; i++) {
+        string += drawLine(i);
+    }
+
+    console.log(string);
+    }
 
 
 module.exports = drawStairs;
