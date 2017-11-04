6cd0b4d670706cc4dbad6cecc42f389fc63c180c
byanofsky <byanofsky@me.com>
2017-11-04 09:59:40 -0700

    Refactor so mean and mode are calculated at same time

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -13,29 +13,19 @@
 function modemean(array) {
   // Track count of numbers seen
   const seen = {};
+  // Track mode
+  let mode = null;
   // Loop through to get mean, and at same time count each number seen
+  // and check if it should replace mode
   const mean = Math.floor(array.reduce((a, b) => {
     if (typeof b !== 'number') throw new Error(`Element ${b} is not a number`);
     if (!(b in seen)) seen[b] = 0;
     seen[b] += 1;
+    if (seen[b] > 1 && mode === null) mode = b;
+    if (seen[b] > 1 && seen[b] > seen[mode]) mode = b;
+    if (seen[b] > 1 && seen[b] === seen[mode] && b > mode) mode = b;
     return a + b;
   }, 0) / (array.length || 1)); // If length === 0, use 1 so we avoid NaN
-  // Get the key (number) and value (times) of most frequent number.
-  // If there isn't one, it is null
-  const modeArr = Object.entries(seen).reduce((mode, [num, times]) => {
-    // If there is no mode and times not greater than 1
-    if (mode === null && !(times > 1)) return null;
-    // If there isn't a mode times > 1, save current to mode
-    if (mode === null) return [num, times];
-    // If there is a mode and current times > mode's times, save current
-    if (times >= mode[1]) return [num, times];
-    // Else, just return what we have
-    return mode;
-  }, null);
-  // From mode array, get mode num if mode is not null
-  const mode = modeArr === null ? null : Number(modeArr[0]);
-  // console.log(mean, mode, seen);
-  // Check if mean and mode are equal
   return mean === mode;
 }
 
