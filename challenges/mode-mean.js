c22e38af5c1078f5df00898291b327bcefbb8600
ryanlondon <ryel590@gmail.com>
2017-11-04 09:49:49 -0700

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,29 @@
  *
  */
 
-
 function modemean(array) {
+  
+  function sum(x, y) {
+    return x + y;
+  }
 
+  const mean = Math.floor(array.reduce(sum) / array.length);
+
+  const modeObj = array.reduce((acc, num) => {
+    acc[num] ? acc[num]++ : acc[num] = 1;
+    return acc;
+  }, {});
+
+  let mode;
+  let occurrences = 1;
+  
+  for (let key in modeObj) {
+    if (modeObj[key] > occurrences) {
+      occurrences = modeObj[key];
+      mode = parseInt(key);
+    }
+  }
+  return mean === mode;
 }
 
 module.exports = modemean;
