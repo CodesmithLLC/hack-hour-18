66316671190949a5e4af9535fee6ecfc1837bb86
eric <ericcodesweb@gmail.com>
2017-11-04 09:49:09 -0700

    mode-mean hack hour

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,38 @@
 
 
 function modemean(array) {
+    let mean;
+    let mode;
+    let modeArr;
+    const cache = [];
 
+    // find mean (average)
+    mean = Math.floor(array.reduce((acc, cur) => {
+        return acc + cur;
+    }, 0) / array.length);
+
+    // If mean is negative, return false;
+    if (mean < 0) return false;
+
+    // find mode (most frequent)
+    array.forEach((item) => {
+        if (cache[item] === undefined) {
+            cache[item] = 1;
+        } else {
+            cache[item] = cache[item] + 1;
+        }
+    });
+
+    modeTimes = -1;
+    for (let i=0; i < cache.length; i++) {
+        if (modeTimes <= cache[i]) {
+            modeTimes = cache[i];
+        }
+    }
+
+    mode = cache.indexOf(modeTimes);
+
+    return mean === mode;
 }
 
 module.exports = modemean;
