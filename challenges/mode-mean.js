4d9bbdc9ec4872680bacd4baa8179e6c51fed174
jiminykbob <jikang92@hotmail.com>
2017-11-04 09:56:02 -0700

    (completed) mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,18 @@
  *
  */
 
-
-function modemean(array) {
-
-}
+ function modemean(array) {
+   let mean = 0
+   const cache = {}
+   array.forEach((el) => {
+     mean += el
+     cache[el] ? cache[el]++ : cache[el] = 1
+   })
+   const mode = Object.entries(cache).reduce((big, el) => {
+     if (el[1] > big[1] || el[1] === big[1] && el[0] > big[0]) big = el
+     return big
+   })
+   return Math.floor(mean/array.length) === mode[1]
+ }
 
 module.exports = modemean;
