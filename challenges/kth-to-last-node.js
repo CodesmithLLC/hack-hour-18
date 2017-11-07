438cc3c57ad7e2f74385034ad1b4005538effa44
Henry Au <henryau01@gmail.com>
2017-11-07 09:38:26 -0800

    Finished kth to last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,34 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  if (typeof k !== 'number') return head.value;
+  if (head !== Object(head)) return undefined;
+  let current = head;
+  let length = 0;
+  let i = 0;
+  while (current) {
+    length += 1;
+    current = current.next;
+  }
+  current = head;
+  while (i < length - k) {
+    i += 1;
+    current = current.next;
+  }
+  return current.value;
 }
+
+const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+// console.log(kthToLastNode('hi', a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
