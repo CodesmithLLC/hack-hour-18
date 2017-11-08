eb73e0ca821221627e84c17d4b8ebd8127d2b3f6
LazarusCrown <azragcapra@protonmail.com>
2017-11-08 09:57:30 -0800

    found a recursive way at last

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,31 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+ function pow(base, power) {
+  return power === 0? 1 : base * pow(base, power-1);
 }
+
+// function pow(base, power) {
+//     let count = 0;
+//     while(count!==power){
+//         base = base*base;
+//         count++;
+//     }
+//     return base;
+// }
+
+// let count = 0;
+
+// function pow(base,power) {
+//   if(power === 0){
+//       return base;
+//   }else if(count !== power){
+//     base = base*base;
+//     count++;
+//     pow();
+//   }else{
+//     return base;
+//   }   
+// }
 
 module.exports = pow;
