06d536b2f510f3b3f0359f16a75e388ea5258861
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-08 09:25:20 -0800

    hack hour pow(er)

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,10 @@
  */
 
 function pow(base, power) {
-
+  if (typeof base !== 'number' || typeof power !== 'number') return null;
+  if (power === 0) return 0;
+  if (power === 1) return base;
+  else return base * (pow(base, power - 1));
 }
 
 module.exports = pow;
