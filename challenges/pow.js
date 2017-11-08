bdf3b000c209a1f9f33e58bfd9717b1500fa67c1
byanofsky <byanofsky@me.com>
2017-11-08 09:37:52 -0800

    Comment out tests

--- challenges/pow.js
+++ challenges/pow.js
@@ -18,22 +18,22 @@ function pow(base, power, value = 1) {
   return pow(base, power - 1, result);
 }
 
-console.log('starting tests');
-
-console.assert(pow(2, 0) === 1);
-console.assert(pow(2, 1) === 2);
-console.assert(pow(2, 2) === 4);
-console.assert(pow(2, 3) === 8);
-
-console.assert(pow(3, 0) === 1);
-console.assert(pow(3, 1) === 3);
-console.assert(pow(3, 2) === 9);
-
-console.assert(pow(2, -1) === 0.5);
-console.assert(pow(2, -2) === 0.25);
-console.assert(pow(2, -3) === 0.125);
-
-console.assert(pow(8, -1) === 0.125);
-console.assert(pow(8, -2) === 0.015625);
+// console.log('starting tests');
+//
+// console.assert(pow(2, 0) === 1);
+// console.assert(pow(2, 1) === 2);
+// console.assert(pow(2, 2) === 4);
+// console.assert(pow(2, 3) === 8);
+//
+// console.assert(pow(3, 0) === 1);
+// console.assert(pow(3, 1) === 3);
+// console.assert(pow(3, 2) === 9);
+//
+// console.assert(pow(2, -1) === 0.5);
+// console.assert(pow(2, -2) === 0.25);
+// console.assert(pow(2, -3) === 0.125);
+//
+// console.assert(pow(8, -1) === 0.125);
+// console.assert(pow(8, -2) === 0.015625);
 
 module.exports = pow;
