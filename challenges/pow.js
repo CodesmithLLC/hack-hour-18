38470bdf562c7c60e36acfb0102f2c251b68ef93
ryanlondon <ryel590@gmail.com>
2017-11-08 09:47:53 -0800

    submitting pow for testing

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,11 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, total = base) {
+	if (typeof base !== 'number' || typeof power !== 'number') return NaN;
+	if (power === 0) return 1;
+	if (power === 1) return total;
+  return pow(base, power - 1, base * total);
 }
 
 module.exports = pow;
