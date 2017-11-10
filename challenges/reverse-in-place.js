b0cf90644e5ed5e912b4d002a3ac1e5443b1846d
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-10 10:01:15 -0800

    reverse

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,15 @@
  */
 
 function reverseInPlace(array) {
+  let temp;
+  let end = array.length - 1;
 
+  for (let i = 0; i < Math.floor((array.length)/2); i += 1) {
+    temp = array[i];
+    array[i] = array[(end) - i];
+    array[(end) - i] = temp;
+  }
+  return array;
 }
 
 module.exports = reverseInPlace;
