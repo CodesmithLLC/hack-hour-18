143a66a37a98c91c2666ed9ecfec77ed6a592234
Peter Lee <peter.hbyul.lee@gmail.com>
2017-11-10 09:52:38 -0800

    (complete)reverseinplace

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,8 +13,11 @@
  * DO NOT USE THE BUILT IN REVERSE METHOD
  */
 
-function reverseInPlace(array) {
-
-}
+ function reverseInPlace(array) {
+   for (let i = 0 ; i < (array.length -1)/2 ; i++) {
+     [array[i], array[array.length-1-i]] = [array[(array.length-1-i)] , array[i]]
+   }
+   return array
+ }
 
 module.exports = reverseInPlace;
