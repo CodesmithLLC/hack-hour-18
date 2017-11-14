482fea2d1665bebf9c067df6198f083bbc30b894
Henry Au <henryau01@gmail.com>
2017-11-14 09:27:52 -0800

    Finished balanced parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -9,7 +9,7 @@
  *   balancedParens('(())');  // true
  *
  * Step 2:
- *   make your solution work for all types of brackets
+ *   make your solution work for all types of braces
  *
  * Example:
  *  balancedParens('[](){}'); // true
@@ -24,8 +24,34 @@
  *
  */
 
-function balancedParens(input){
+const braces = { '{': '}', '[': ']', '(': ')' };
 
+function isOpenBrace(element) {
+  return braces[element];
 }
+
+function isCloseBrace(element) {
+  return Object.values(braces).indexOf(element) !== -1;
+}
+
+function balancedParens(input) {
+  if (typeof input !== 'string') return undefined;
+  const braceStack = [];
+  const inputArr = input.split('');
+  for (let i = 0; i < inputArr.length; i++) {
+    if (isOpenBrace(inputArr[i])) braceStack.push(inputArr[i]);
+    else if (braceStack.length !== 0 && inputArr[i] === braces[braceStack[braceStack.length - 1]]) braceStack.pop();
+    else if (isCloseBrace(inputArr[i])) return false;
+  }
+  return braceStack.length === 0;
+}
+
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')) // true
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();')) // false
+// console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]')); // true
+// console.log(balancedParens('[(]{)}')); // false
+// console.log(balancedParens(7)); // false
+
 
 module.exports = balancedParens;
