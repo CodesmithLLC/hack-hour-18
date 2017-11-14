81b40e6309847ca11a727d3ca0c1445e1b2c69d2
byanofsky <byanofsky@me.com>
2017-11-14 10:22:07 -0800

    Adjust balanced parens to a better algo

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,53 +24,68 @@
  *
  */
 
+// function balancedParens(input) {
+//   for (let pos = 0; pos < input.length; pos += 1) {
+//     if ('({['.indexOf(input[pos]) > -1) {
+//       const result = isBalanced(input, pos);
+//       if (result === false) return false;
+//       pos = result;
+//     } else if (')}]'.indexOf(input[pos]) > -1) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+//
+// function isBalanced(input, initPos) {
+//   const startChar = '({[';
+//   const endChar = ')}]';
+//   const charI = startChar.indexOf(input[initPos]);
+//   // if (charI === -1) return false;
+//   for (let pos = initPos + 1; pos < input.length; pos += 1) {
+//     const curChar = input[pos];
+//     // Reached closing paren
+//     if (curChar === endChar[charI]) return pos;
+//     // Reached another opening paren
+//     if (startChar.indexOf(curChar) > -1) {
+//       pos = isBalanced(input, pos);
+//       if (pos === false) return false;
+//     }
+//     // Reached a closing paren that doesn't match the current type
+//     else if (')}]'.indexOf(curChar) !== -1) {
+//       return false;
+//     }
+//   }
+//   return false;
+// }
+
 function balancedParens(input) {
-  for (let pos = 0; pos < input.length; pos += 1) {
-    if ('({['.indexOf(input[pos]) > -1) {
-      const result = isBalanced(input, pos);
-      if (result === false) return false;
-      pos = result;
-    } else if (')}]'.indexOf(input[pos]) > -1) {
-      return false;
+  const parens = { '(': ')', '{': '}', '[': ']' };
+  const parensStack = [];
+  for (let i = 0; i < input.length; i += 1) {
+    const cur = input[i];
+    if (cur in parens) {
+      parensStack.push(parens[cur]);
+    }
+    if (cur === parensStack[parensStack.length - 1]) {
+      parensStack.pop();
     }
   }
-  return true;
+  return parensStack.length === 0;
 }
 
-function isBalanced(input, initPos) {
-  const startChar = '({[';
-  const endChar = ')}]';
-  const charI = startChar.indexOf(input[initPos]);
-  // if (charI === -1) return false;
-  for (let pos = initPos + 1; pos < input.length; pos += 1) {
-    const curChar = input[pos];
-    // Reached closing paren
-    if (curChar === endChar[charI]) return pos;
-    // Reached another opening paren
-    if (startChar.indexOf(curChar) > -1) {
-      pos = isBalanced(input, pos);
-      if (pos === false) return false;
-    }
-    // Reached a closing paren that doesn't match the current type
-    else if (')}]'.indexOf(curChar) !== -1) {
-      return false;
-    }
-  }
-  return false;
-}
+console.assert(balancedParens('(') === false);  // false
+console.assert(balancedParens('()') === true); // true
+console.assert(balancedParens(')(') === false);  // false
+console.assert(balancedParens('(())') === true);  // true
+console.assert(balancedParens('(()') === false);  // false
 
-// console.assert(balancedParens('(') === false);  // false
-// console.assert(balancedParens('()') === true); // true
-// console.assert(balancedParens(')(') === false);  // false
-// console.assert(balancedParens('(())') === true);  // true
-// console.assert(balancedParens('(()') === false);  // false
-//
-// console.assert(balancedParens('[](){}') === true); // true
-// console.assert(balancedParens('[}') === false); // false
-// console.assert(balancedParens('[({})]') === true);   // true
-// console.assert(balancedParens('[(]{)}') === false); // false
-//
-// console.assert(balancedParens(' var wow  = { yo: thisIsAwesome() }') === true); // true
-// console.assert(balancedParens(' var hubble = function() { telescopes.awesome();') === false); // false
+console.assert(balancedParens('[](){}') === true); // true
+console.assert(balancedParens('[}') === false); // false
+console.assert(balancedParens('[({})]') === true);   // true
+console.assert(balancedParens('[(]{)}') === false); // false
+
+console.assert(balancedParens(' var wow  = { yo: thisIsAwesome() }') === true); // true
+console.assert(balancedParens(' var hubble = function() { telescopes.awesome();') === false); // false
 
 module.exports = balancedParens;
