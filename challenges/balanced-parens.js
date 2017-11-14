3a059e8d46e932a489376b4cda9ef88250a3dfcf
eric <ericcodesweb@gmail.com>
2017-11-14 09:31:08 -0800

    balancedParens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,57 @@
  */
 
 function balancedParens(input){
+    let begin = 0;
+    let end = input.length -1;
+    const brackets = ['[', ']', '(', ')'];
+    let found = false;
 
+    for (let i=0; i< input.length; i++) {
+        if (input[i] === '(') {
+            let j = i + 1;
+            let finished = false;
+            while (!finished) {
+                if (j = input.length) return false;
+                if (input[j] === ')') {
+                    i = j;
+                    finished = true;
+                } else if (input[j] === ']' || input[j] === '}') {
+                    return false;
+                }
+                j++;
+            }
+            
+        }
+        if (input[i] === '[') {
+            let j = i + 1;
+            let finished = false;
+            while (!finished) {
+                if (j = input.length) return false;
+                if (input[j] === ']') {
+                    i = j;
+                    finished = true;
+                } else if (input[j] === ')' || '}') {
+                    return false;
+                }
+                j++;
+            }
+        }
+        if (input[i] === '{') {
+            let j = i + 1;
+            let finished = false;
+            while (!finished) {
+                if (j = input.length) return false;
+                if (input[j] === '}') {
+                    i = j;
+                    finished = true;
+                } else if (input[j] === ')' || ']') {
+                    return false;
+                }
+                j++;
+            }
+        }
+    }
+    return true;
 }
 
 module.exports = balancedParens;
