2292b1e139d5d6d83da4cb39eb5c77195220a44f
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-14 10:01:22 -0800

    (unfinished) balanced parens hack hour

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,39 @@
  *
  */
 
+// solution will not work for '[](){}';
 function balancedParens(input){
+  if (typeof input !== 'string') return;
+  let newStr = '';
 
+  // try to use regex to only match brackets
+  input.split('').forEach(elem => {
+    if (elem === '(' || elem === '{' || elem === '[' || elem === ']' || elem === '}' || elem === ')') {
+      newStr += elem;
+    }
+  });
+
+  // get 2 halves of bracket strings
+  const str1 = newStr.slice(0, Math.floor(newStr.length / 2));
+  const str2 = newStr.slice(Math.floor(newStr.length / 2), newStr.length);
+
+
+  function compare(x, y) {
+    if (x === '(' && y !== ')') {
+      return false;
+    } else if (x === '{' && y !== '}') {
+      return false
+    } else if (x === '[' && y !== ']') {
+      return false;
+    }
+    return true;
+  }
+
+  for (let i = 0; i < str1.length; i += 1) {
+    for (let j = str2.length - 1; j >= 0; j -= 1) {
+      return compare(str1[i], str2[j]);
+    }
+  }
 }
 
 module.exports = balancedParens;
