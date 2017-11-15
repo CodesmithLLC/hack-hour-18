3c6fdeb3ea11b5b1b9371654118b7663fc58443c
ryanlondon <ryel590@gmail.com>
2017-11-15 10:01:37 -0800

    wed commit

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,42 @@
  * 
  */
 
-function romanNumeral(n) {
+function romanize(str, place) {
+  let returnStr = '';
 
+  const thousands = { '1': 'M', 'x': '' };
+  const hundreds = { '1': 'C', '5': 'D', '0': 'M', 'x': ''};
+  const tens = { '1': 'X', '5': 'L', '0': 'C', 'x': '' };
+  const ones = { '1': 'I', '5': 'V', '0': 'X', 'x': '' };
+
+  for (let i = 0; i < str.length; i++) {
+    if (place === 4) {
+      returnStr += thousands[str[i]];
+    } else if (place === 3) {
+      returnStr += hundreds[str[i]];
+    } else if (place === 2) {
+      returnStr += tens[str[i]];
+    } else {
+      returnStr += ones[str[i]];
+    }
+  }
+  return returnStr;
+}
+
+function romanNumeral(n) {
+  const arr = n.toString().split('');
+  const places = arr.length;
+  let romanStr = '';
+
+  const format = { '1': '1', '2': '11', '3': '111', '4': '15', '5': '5', '6': '51', '7': '511', '8': '5111', '9': '10', '0': 'x' };
+  
+
+  for (let i = places - 1; i >= 0; i--) {
+    romanStr += romanize(format[arr[i]], i + 1);
+  }
+
+  return romanStr;
 }
 
 module.exports = romanNumeral;
+
