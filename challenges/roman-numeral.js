36f4cd27b05f446d1d9776b8352f1c9cc0da692b
jiminykbob <jikang92@hotmail.com>
2017-11-15 09:33:18 -0800

    finish romannumerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,19 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
-
+  const table = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+  let sum = 0;
+  let temp;
+  for (let i = 0; i < n.length; i++) {
+    if (table[n[i]] > temp) sum += table[n[i]] - temp - temp;
+    else sum += table[n[i]];
+    temp = table[n[i]];
+  }
+  return sum;
 }
 
 module.exports = romanNumeral;
