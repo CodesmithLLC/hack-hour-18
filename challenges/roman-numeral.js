6b537837edaa0cf97935ee91ff59e3393bcda485
ryanlondon <ryel590@gmail.com>
2017-11-17 23:34:27 -0800

    roman fixed for posterity

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,42 +17,29 @@
  * 
  */
 
-function romanize(str, place) {
-  let returnStr = '';
-
-  const thousands = { '1': 'M', 'x': '' };
-  const hundreds = { '1': 'C', '5': 'D', '0': 'M', 'x': ''};
-  const tens = { '1': 'X', '5': 'L', '0': 'C', 'x': '' };
-  const ones = { '1': 'I', '5': 'V', '0': 'X', 'x': '' };
-
-  for (let i = 0; i < str.length; i++) {
-    if (place === 4) {
-      returnStr += thousands[str[i]];
-    } else if (place === 3) {
-      returnStr += hundreds[str[i]];
-    } else if (place === 2) {
-      returnStr += tens[str[i]];
-    } else {
-      returnStr += ones[str[i]];
-    }
-  }
-  return returnStr;
-}
-
 function romanNumeral(n) {
-  const arr = n.toString().split('');
-  const places = arr.length;
+  const values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  const numerals = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
   let romanStr = '';
 
-  const format = { '1': '1', '2': '11', '3': '111', '4': '15', '5': '5', '6': '51', '7': '511', '8': '5111', '9': '10', '0': 'x' };
-  
-
-  for (let i = places - 1; i >= 0; i--) {
-    romanStr += romanize(format[arr[i]], i + 1);
-  }
-
+  values.reduce((total, value, i) => {
+    while (total >= value) {
+      romanStr += numerals[i];
+      total -= value;
+    }
+    return total;
+  }, n);
   return romanStr;
 }
 
+// romanNumeral(1);
+// romanNumeral(4);
+// romanNumeral(11);
+// romanNumeral(99);
+// romanNumeral(134);
+// romanNumeral(500);
+// romanNumeral(1000);
+// romanNumeral(899);
+// romanNumeral(5);
 module.exports = romanNumeral;
 
