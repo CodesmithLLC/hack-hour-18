06db82a27a1e2aecd65738a47e49d6dff0179e5e
Justin Ko <justinko@Justins-MacBook-Pro-2.local>
2017-11-18 09:41:53 -0800

    finished bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,20 @@
  */
 
 function binToDec(binary) {
+    // const obj = {'0': 1,'1': 2,'2': 4,'3': 8,'4': 16,'5': 32,'6': 64,'7': 128, '8':256, '9': 512, '10': 1024, '11':2048};
+    let obj = {};
+    for (let i = 0; i < binary.length; i++){
+        obj[i] = Math.pow(2, i);
+    }
 
+    const binArr = binary.split('').reverse();
+    let returnVal = 0;
+    binArr.forEach((element, index) => {
+        if (binArr[index] === '1'){
+            returnVal += obj[index];
+        }
+    });
+    return returnVal;
 }
 
 module.exports = binToDec;
