0c77b37c4fca791a9e0bba1585b5f4234fafc86f
ryanlondon <ryel590@gmail.com>
2017-11-18 10:00:17 -0800

    bin to dec finished

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,18 @@
  */
 
 function binToDec(binary) {
-
+  return binary.split('').reduce((total, digit, i) => {
+    return digit === '1' ? total += Math.pow(2, binary.length - (i + 1)) : total;
+  }, 0);
 }
+
+console.log(binToDec('0'))   // -> 0
+console.log(binToDec('11'))  // -> 3
+console.log(binToDec('100')) // -> 4
+console.log(binToDec('101')) // -> 5
+console.log(binToDec('0101')) // -> 5
+console.log(binToDec('10101')) // -> 21
+console.log(binToDec('110101')) // -> 53
+console.log(binToDec('11111111')) // -> 255
 
 module.exports = binToDec;
