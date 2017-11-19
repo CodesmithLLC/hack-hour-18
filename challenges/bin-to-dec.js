80dacedc70a9de738ed370b2f9c66ea7c597d544
byanofsky <byanofsky@me.com>
2017-11-18 17:13:55 -0800

    Complete bin to dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,20 @@
  */
 
 function binToDec(binary) {
-
+  let total = 0;
+  for (let i = 0; i < binary.length; i += 1) {
+    const pos = binary.length - 1 - i;
+    total += binary[pos] * (2 ** i);
+  }
+  return total;
 }
+
+// console.assert(binToDec('0') === 0);
+// console.assert(binToDec('11') === 3);
+// console.assert(binToDec('100') === 4);
+// console.assert(binToDec('101') === 5);
+// console.assert(binToDec('0101') === 5);
+// console.assert(binToDec('1000') === 8);
+// console.assert(binToDec('1001') === 9);
 
 module.exports = binToDec;
