9f90fa82a6a89f21baf27479e83d91e4a2e19ef9
byanofsky <byanofsky@me.com>
2017-11-22 09:58:10 -0800

    More efficient solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,29 +2,44 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
+// function highestProduct(array) {
+//   if (!(array.length >= 3) || !Array.isArray(array)) return null;
+//   let largestSoFar = -Infinity;
+//   array.forEach((firstMult, i) => {
+//     // console.log('First', firstMult);
+//     array.slice(i + 1).forEach((secondMult, j) => {
+//       // console.log('Second', secondMult);
+//       array.slice(j + i + 1 + 1).forEach((thirdMult) => {
+//         // console.log('Third', thirdMult);
+//         largestSoFar = Math.max(largestSoFar, firstMult * secondMult * thirdMult);
+//       });
+//     });
+//   });
+//   return largestSoFar;
+// }
+
 function highestProduct(array) {
   if (!(array.length >= 3) || !Array.isArray(array)) return null;
-  let largestSoFar = -Infinity;
-  array.forEach((firstMult, i) => {
-    // console.log('First', firstMult);
-    array.slice(i + 1).forEach((secondMult, j) => {
-      // console.log('Second', secondMult);
-      array.slice(j + i + 1 + 1).forEach((thirdMult) => {
-        // console.log('Third', thirdMult);
-        largestSoFar = Math.max(largestSoFar, firstMult * secondMult * thirdMult);
-      });
-    });
-  });
-  return largestSoFar;
+  array.sort((a, b) => b - a);
+  if (array[array.length - 1] < 0 && array[array.length - 2] < 0) {
+    const final = -1 * array.pop();
+    const seclast = -1 * array.pop();
+    array.push(final, seclast);
+    array.sort((a, b) => b - a);
+  }
+  return array.slice(0, 3).reduce((a, c) => a * c);
 }
 
-console.assert(highestProduct([]) === null);
-console.assert(highestProduct([1, 2, 3]) === 6);
-console.assert(highestProduct([1, 2, 3, 4]) === 24);
-console.assert(highestProduct([4, 2, 1, 3]) === 24);
-console.assert(highestProduct([4, 2, 1, 3, 5]) === 60);
-console.assert(highestProduct([4, 2, 10, 3, 5]) === 200);
-console.assert(highestProduct([4, 2, -10, 3, -5]) === 200);
+// console.assert(highestProduct([]) === null);
+// console.assert(highestProduct([1, 2, 3]) === 6);
+// console.assert(highestProduct([1, 2, 3, 4]) === 24);
+// console.assert(highestProduct([4, 2, 1, 3]) === 24);
+// console.assert(highestProduct([4, 2, 1, 3, 5]) === 60);
+// console.assert(highestProduct([4, 2, 10, 3, 5]) === 200);
+// // console.log(highestProduct([4, 2, 10, 3, 5]));
+// console.assert(highestProduct([4, 2, -10, 3, -5]) === 200);
+// // console.log(highestProduct([4, 2, -10, 3, -5]));
+// console.assert(highestProduct([-4, 2, -10, 3, -5]) === 150);
 
 
 module.exports = highestProduct;
