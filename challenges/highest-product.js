1dd0ccb949942cbacd0dee4fe85b08adff55db15
Henry Au <henryau01@gmail.com>
2017-11-22 09:43:42 -0800

    Finished highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,12 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length === 0) return 0;
+  const tempArr = [...array].sort((a, b) => a < b);
+  if (tempArr.length < 3) tempArr.push(1, 1, 1);
+  return tempArr[0] * tempArr[1] * tempArr[2];
 }
 
+// console.log(highestProduct([1, 2]));
 
 module.exports = highestProduct;
