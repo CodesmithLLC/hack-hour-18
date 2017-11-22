2f49cafdcfec365754dc1cc86f4679371ca2f648
Tiffany Lin <tiffanylin@Andrew-Chan.local>
2017-11-22 10:23:00 -0800

    refactored highest prod

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,12 +3,23 @@
  */
 
 function highestProduct(array) {
-  if (!Array.isArray(array) || array.length < 3) return 0;
+  if (array.length < 3 || !Array.isArray(array)) return 0;
   
   const sorted = array.sort((a, b) => {
     return a - b;
   });
   const threeNums = sorted.slice(sorted.length-3);
+  const negs = [];
+  for (let i = 0; i < threeNums.length; i++) {
+    if (threeNums[i] < 0) {
+      negs.push(threeNums[i]);
+    }
+  }
+  if (negs.length % 3 === 0) {
+    return negs.reduce(function(accum, val) {
+    return (accum) * (val);
+  })
+  }
   return threeNums.reduce(function(accum, val) {
     return Math.abs(accum) * Math.abs(val);
   })
