85cf5ab57d92f980a57d4524a0121690bbca37bd
ryanlondon <ryel590@gmail.com>
2017-11-22 09:55:49 -0800

    added highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,31 @@
  */
 
 function highestProduct(array) {
-
+  if (!array.length) return 0;
+  return array.reduce((acc, num) => {
+    if (acc.length < 3) {
+      acc.push(num);
+      return acc;
+    } else {
+    	for (let i = 0; i < acc.length; i++) {
+      	if (num > acc[i]) {
+        	acc[i] = num;
+        	break;
+      	}
+    	}
+    }
+    return acc.sort((a,b) => a - b);
+  }, [])
+  .reduce((product, num) => {
+  	return product * num;
+  }, 1);
 }
 
+
+console.log(highestProduct([1,2,3]));
+console.log(highestProduct([1,2,3,4]));
+console.log(highestProduct([1,2,3,4,5,6]));
+console.log(highestProduct([]));
+console.log(highestProduct([1]));
 
 module.exports = highestProduct;
