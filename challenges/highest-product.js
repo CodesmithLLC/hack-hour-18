6ead1135fb97397abda72fb4e4e81573a1d61143
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-22 10:00:13 -0800

    hack hour highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,14 @@
  */
 
 function highestProduct(array) {
+  if (!Array.isArray(array) || []) return 0;
 
+  const sortedNums = array.sort((a, b) => a - b);
+  const highestNums = sortedNums.length > 3 ? sortedNums.slice(sortedNums.length - 3, sortedNums.length) : sortedNums;
+
+  return highestNums.reduce((acc, cur) => {
+    return acc *= cur;
+  });
 }
-
 
 module.exports = highestProduct;
