218138db57cdc4013fb199184d5cfc35dfa99d04
Peter Lee <peter.hbyul.lee@gmail.com>
2017-11-22 10:15:29 -0800

    (redo)highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,30 @@
  */
 
  function highestProduct(array) {
-   if (array.length < 3) return 'error XD'
+   if (array.length < 3 || !Array.isArray(array)) return 0
+
    array.sort((a, b) => b - a)
+
+   let endIndex = array.length-1
+   let zeroes = 0
+   let positives = 0
+   let negatives = 0
+   array.forEach(num => {
+     if (!num) zeroes ++
+     if (num < 0) negatives++
+     if (num > 0) positives++
+   })
+
+   if (!positives) {
+     if (zeroes) return 0
+     return array[0] * array[1] * array[2]
+   }
+
+   if (negatives > 1) {
+     array[endIndex] *= -1
+     array[endIndex-1] *= -1
+     array.sort((a, b) => b - a)
+   }
    return array[0] * array[1] * array[2]
  }
 
