210816ae6f5be050eefc24dc29e94b9710d972fd
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2017-11-22 10:20:26 -0800

    updated highest-product to work with negatives

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -5,22 +5,13 @@
 function highestProduct(array) {
   if (array.length < 3) return 0;
   if (!Array.isArray(array)) return 0;
-  const maxes = [];
-  while (maxes.length < 3) {
-    let max = findMax(array);
-    maxes.push(max);
-    let index = array.indexOf(max);
-    array.splice(index, 1);
+  const sorted = array.sort((a, b) => a > b);
+  const maxes = [sorted[sorted.length - 3], sorted[sorted.length - 2], sorted[sorted.length - 1]];
+  if ((sorted[0] * sorted[1]) > maxes[0] * maxes[1]) {
+    maxes[0] = sorted[0];
+    maxes[1] = sorted[1];
   }
-  return maxes.reduce((accum, num) => accum * num);
-}
-
-function findMax(array) {
-  let max = -Infinity;
-  array.forEach(num => {
-    if (num > max) max = num;
-  });
-  return max;
+  return maxes.reduce((accum, num) => num * accum);
 }
 
 module.exports = highestProduct;
