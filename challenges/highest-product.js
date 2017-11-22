a4b16242a107df98d946f471601a748bac2095dd
Ringo Yip <ringoyip0901@gmail.com>
2017-11-22 13:20:53 -0800

    hack hour

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,24 +3,32 @@
  */
 
 function highestProduct(array) {
-	if (array.length < 3) {return 0}
 	if (!Array.isArray(array)) {
 		return 0;
 	}
-	var prod = 1; 
-	var max = prod;
-	for (let i = 0; i < array.length; i++) {
-	  for (let j = i + 1; j < array.length; j++) {
-	    for (let k = j + 1; k < array.length; k++) {
-	      prod = array[i] * array[j] * array[k];
-	      if (prod > max) {
-	        max = prod
-	      }
-	    }
-	  }
-	}
-	return max;
+	if (array.length < 3) {return 0}
+	if (array.length === 3) {return array.reduce((a,b) =>  (a * b))}
+// 	var prod = 1; 
+// 	var max = 1;
+// 	for (let i = 0; i < array.length; i++) {
+// 	  for (let j = i + 1; j < array.length; j++) {
+// 	    for (let k = j + 1; k < array.length; k++) {
+// 	      prod = array[i] * array[j] * array[k];
+// 	      if (prod > max) {
+// 	        max = prod
+// 	      }
+// 	    }
+// 	  }
+// 	}
+// 	return max;
+  var sort = array.sort((a,b) => (b - a))
+  if (sort[sort.length-1]  < 0 && sort[sort.length-2] < 0) {
+    return sort[sort.length-1] * sort[sort.length-1] * sort[0];
+  }
+  else {
+    return sort[0] * sort[1] * sort[2]
+  }
 }
 
-
+//this solution does not work for negative numbers
 module.exports = highestProduct;
