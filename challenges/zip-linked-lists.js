ab26f6eea73dce67e26484d072acb573062eb96a
eric <ericcodesweb@gmail.com>
2017-11-28 09:42:35 -0800

    zip linked lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,27 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (l1 === undefined) return l2;
+  if (l2 === undefined) return l1;
+  let list1 = l1;
+  let list2 = l2;
+  let tmp;
+  let turn = 1;
+
+  while (tmp1 !== null && tmp2 !== null) {
+    if (turn === 1) {
+      tmp = list1.next;
+      list1.next = list2;
+      list1 = tmp;
+      turn = 2;
+    } else {
+      tmp = list2.next;
+      list2.next = list1;
+      list2 = tmp;
+      turn = 1;
+    }
+  }
+
 };
 
 module.exports = {Node: Node, zip: zip};
