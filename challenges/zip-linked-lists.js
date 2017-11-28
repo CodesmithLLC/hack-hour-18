e1aaba5f0315b4af22a864dbfca96c5d5639b8ee
byanofsky <byanofsky@me.com>
2017-11-28 09:44:27 -0800

    Refactor a bit

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -15,18 +15,17 @@ function zip(l1, l2) {
   let cur = head;
   let cur1 = l1.next;
   let cur2 = l2;
-  let is2 = true;
+  let useL2 = true;
   while (cur1 && cur2) {
-    if (is2) {
+    if (useL2) {
       cur.next = cur2;
-      cur = cur.next;
       cur2 = cur2.next;
     } else {
       cur.next = cur1;
-      cur = cur.next;
       cur1 = cur1.next;
     }
-    is2 = !is2;
+    cur = cur.next;
+    useL2 = !useL2;
   }
   if (cur1) {
     cur.next = cur1;
@@ -37,21 +36,21 @@ function zip(l1, l2) {
   return head;
 }
 
-// const n1 = new Node(1);
-// const n2 = new Node(2);
-// const n3 = new Node(3);
-// const n4 = new Node(4);
-// const n5 = new Node(5);
-// const na = new Node('a');
-// const nb = new Node('b');
-//
-// n1.next = n2;
-// n2.next = n3;
-// n3.next = n4
-// n4.next = n5;
-//
-// na.next = nb;
-//
-// console.dir(zip(na, n1));
+const n1 = new Node(1);
+const n2 = new Node(2);
+const n3 = new Node(3);
+const n4 = new Node(4);
+const n5 = new Node(5);
+const na = new Node('a');
+const nb = new Node('b');
+
+n1.next = n2;
+n2.next = n3;
+n3.next = n4
+n4.next = n5;
+
+na.next = nb;
+
+console.dir(zip(na, n1));
 
 module.exports = {Node: Node, zip: zip};
