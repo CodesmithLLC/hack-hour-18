6390f70d0954efedc8bbc1cf456128601bc258be
jiminykbob <jikang92@hotmail.com>
2017-11-29 09:33:37 -0800

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,26 @@
 
 function Stack() {
   // body...
+  this.array = [];
+  this.index = 0;
 }
+
+Stack.prototype.push = function(el) {
+  this.array[index] = el;
+  this.index += 1;
+  return this.array.length;
+};
+
+Stack.prototype.pop = function(el) {
+  this.index -= 1;
+  return this.array.splice(index, 1);
+};
+
+Stack.prototype.getMax = function(el) {
+  return array.reduce((greatest, el) => {
+    if (el > greatest) acc = el;
+    return greatest;
+  })
+};
 
 module.exports = Stack;
