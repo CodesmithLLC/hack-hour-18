5d972f66ff4d993360dcf2a6121b5b814a5e7eb3
programminPete <peter.fasula@gmail.com>
2017-11-29 09:36:36 -0800

    max-stack attempt1 - trying for O(1) time

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,49 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.index = 0;
 }
+
+function Stack() {
+  this.storage = {};
+  this.index = 0;
+}
+
+Stack.prototype.push = function(val){
+  this.storage[this.index] = val;
+  this.index++;
+  return this.index;
+}
+
+Stack.prototype.pop = function(){
+  let removed = this.storage[this.index--]
+  let tempStorage = {};   // should be able to do this without a temp, I think..
+  for(let i = 0; i < this.index; i++){
+    tempStorage[i] = this.storage[i];
+  }
+  this.storage = tempStorage;
+  // this.index--;
+  return removed
+}
+
+Stack.prototype.getMax = function(){
+  let max = -Infinity;
+  for(let i = 0; i<this.index; i++){
+    if(this.storage[i] > max){
+      max = this.storage[i];
+    }
+  }
+  return max;
+}
+
+// var stackTest = new Stack();
+
+// stackTest.push(1);
+// stackTest.push(2);
+// stackTest.pop();
+// stackTest.push(3);
+// stackTest.push(4);
+// stackTest.push(5);
 
 module.exports = Stack;
