420f70cea6d62bb476f8ce3758e92065eb120095
tipdub <mstephenlin@gmail.com>
2017-11-29 09:59:30 -0800

    still working on it

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,61 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+Stack.prototype.push = function (data) {
+	let size = this.size++
+	this.storage[size] = data;
 }
+
+Stack.prototype.pop = function () {
+	let size = this.size,
+	let poppedData;
+
+	poppedData = this.storage[size];
+
+	delete this.storage[size];
+	this.size--;
+
+	return poppedData;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function Stack() {
+	this.size = 0;
+	this.storage = [];
+}
+
+function MaxStack() {
+  this.stack = new Stack();
+  this.maxStack = new Stack();
+}
+
+MaxStack.prototype.push = function(item) {
+	let size = this.size++
+	this.storage[size] = data;
+
+	var currentMax = this.maxStack.peek();
+	if (!currentMax || item >= currentMax) {  // this >= is crucial
+    this.maxStack.push(item);
+  }
+  
+  return this.stack.push(item);
+};
+
+MaxStack.prototype.pop = function() {
+  var popped = this.stack.pop(); 
+  if (this.maxStack.peek() === popped) {
+    this.maxStack.pop();
+  }
+  return popped; // this return not necessary 
+};
+
+
+MaxStack.prototype.getMax = function() {
+  return this.maxStack.peek();
+}
+
+
+
 
 module.exports = Stack;
