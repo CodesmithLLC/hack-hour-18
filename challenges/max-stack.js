b6c8cbff1e834a7cbdf093b112981dbeb3cd6eee
eric <ericcodesweb@gmail.com>
2017-11-29 09:29:15 -0800

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,37 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.length = 0;
+  this.max = -Infinity;
+
+  this.push = (el) => {
+    this.storage[this.length] = el;
+    this.length += 1;
+    if (el > this.max) {
+      this.max = el;
+    }
+    return this.length;
+  };
+
+  this.pop = (el) => {
+    const tmp = this.storage[this.length - 1];
+    this.storage[this.length - 1] = undefined;
+    this.length--;
+    if (tmp === this.max) {
+      for (let i=0; i < this.length; i++) {
+        if (this.storage[i] > this.max) {
+          this.max = this.storage[i];
+        }
+      }
+    }
+    return tmp;
+  };
+
+  this.getMax = () => {
+    return this.max;
+  };
 }
+
 
 module.exports = Stack;
