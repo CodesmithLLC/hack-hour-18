95e310e399ac0681e09f5b29ebb3d12623ccaa3a
byanofsky <byanofsky@me.com>
2017-11-29 09:34:08 -0800

    Complete max-stack challenge

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,80 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+function Node(value) {
+  this.value = value;
+  this.next = null;
 }
+
+function Stack() {
+  this.head = null;
+  this.length = 0;
+}
+
+Stack.prototype.push = function push(value) {
+  if (!this.head) {
+    this.head = new Node(value);
+  } else {
+    const tmp = this.head;
+    this.head = new Node(value);
+    this.head.next = tmp;
+  }
+  this.length += 1;
+  return this.length;
+};
+
+Stack.prototype.pop = function pop() {
+  if (!this.head) return null;
+  const removedNode = this.head;
+  this.head = this.head.next;
+  this.length -= 1;
+  return removedNode.value;
+};
+
+Stack.prototype.getMax = function getMax() {
+  if (!this.length) return null;
+  let max = -Infinity;
+  let cur = this.head;
+  while (cur) {
+    if (cur.value > max) max = cur.value;
+    cur = cur.next;
+  }
+  return max;
+};
+
+// Tests
+const s = new Stack();
+
+console.assert(s.length === 0);
+console.assert(s.push(1) === 1);
+console.assert(s.push(2) === 2);
+console.assert(s.push(3) === 3);
+
+console.assert(s.pop() === 3);
+console.assert(s.pop() === 2);
+console.assert(s.pop() === 1);
+console.assert(s.pop() === null);
+console.assert(s.pop() === null);
+
+// Test max
+const s2 = new Stack();
+
+console.assert(s2.getMax() === null);
+s2.push(0);
+s2.push(10);
+s2.push(-5);
+s2.push(-100);
+console.assert(s2.getMax() === 10);
+
+s2.push(100);
+s2.push(-500);
+console.assert(s2.getMax() === 100);
+
+s2.pop();
+s2.pop();
+s2.pop();
+s2.pop();
+s2.pop();
+console.assert(s2.getMax() === 0);
 
 module.exports = Stack;
