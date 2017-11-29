3eda6252383018196e7c7684c5f6b1cd6a9f259e
Kevin Gabinete <kgabinete08@gmail.com>
2017-11-29 09:53:51 -0800

    hack hour stack using array and object

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,55 @@
 
 function Stack() {
   // body...
+  // solution using array
+  // const body = [];
+
+  // this.push = (val) => {
+  //   body.push(val);
+  //   return body.length;
+  // }
+
+  // this.pop = () => {
+  //   return body.pop();
+  // }
+
+  // this.getMax = () => {
+  //   let temp = body[0];
+  //   for (let i = 0; i < body.length; i += 1) {
+  //     if (body[i] > temp) temp = body[i];
+  //   }
+  //   return temp;
+  // }
+
+  // solution using object
+  const body = {};
+  let index = 0;
+  let length = 0;
+
+  this.push = (val) => {
+    body[index] = val;
+    index += 1;
+    length += 1;
+    return length;
+  }
+
+  this.pop = () => {
+    const output = body[index - 1];
+
+    delete body[index - 1];
+    index -= 1;
+    length -= 1;
+    return output;
+  }
+
+  this.getMax = () => {
+    let temp = body[0];
+
+    for (let i = 0; i < length; i += 1) {
+      if (body[i] > temp) temp = body[i];
+    }
+    return temp;
+  }
 }
 
 module.exports = Stack;
