4cb16c2c40d0db0772d4b2767072c7902ac38276
Henry Au <henryau01@gmail.com>
2017-12-01 09:53:42 -0800

    Finished apple-stock

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,8 +12,23 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-function bestProfit(stock_prices_yesterday) {
-
+function bestProfit(prices) {
+  if (prices.length < 2) return 0;
+  let maxProfit = prices[1] - prices[0];
+  let price1 = prices[0];
+  let price2;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i + 1] < prices[i]) {
+      price2 = prices[i];
+      maxProfit = Math.max(maxProfit, price2 - price1);
+      price1 = prices[i + 1];
+    }
+  }
+  return maxProfit;
 }
+
+// const profits = [1, 10, 2, 3, 0, 22, 23, 15];
+
+// console.log(bestProfit(profits));
 
 module.exports = bestProfit;
