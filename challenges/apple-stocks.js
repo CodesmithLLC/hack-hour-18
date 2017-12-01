6938254e53ced4645d84c9491ede4d44ef56e363
tipdub <mstephenlin@gmail.com>
2017-12-01 10:01:06 -0800

    completed

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,7 +12,35 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
+//check what the max is
+//check to see what the min after it is
+//so update the min after each max
+//if a new max, then update the max
 function bestProfit(stock_prices_yesterday) {
+	let minIndex = 0;
+	let maxIndex = 1;
+	let currMinIndex = 0;
+	let maxProfit = 0;
+
+	for (let i=1; i< arr.length; i++){
+		
+		//if the element at index 1 is less than the element at index 0, then update the current Min
+		//otherwise keep it the same, because the element at index 0 is still the lowest
+		//@index1 < @index0
+		//currMinIndex = 1
+		if(arr[i] < arr[currMinIndex]){
+			currMinIndex = i;
+		}
+
+		
+		if (arr[maxIndex] - arr[minIndex] < arr[i] - arr[currMinIndex]){
+			maxIndex = i;
+			minIndex = currMinIndex;
+		}
+	}
+
+	maxProfit = arr[maxIndex] - arr[minIndex];
+	return maxProfit;
 
 }
 
