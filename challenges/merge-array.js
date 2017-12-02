c5cebadae01554b4fecac9deeb1e071a82f2afb2
ryanlondon <ryel590@gmail.com>
2017-12-02 09:52:32 -0800

    merge-array more done

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -19,12 +19,10 @@ function mergeArrays(arr1, arr2) {
   let i2 = 0;
 
   while (i1 < arr1.length || i2 < arr2.length) {
-		if (arr1[i1] && arr2[i2]) {
-			if (arr1[i1] <= arr2[i2]) {
-				newArr.push(arr1[i1++]);
-			} else {
-				newArr.push(arr2[i2++]);
-			}
+		if (arr2[i2] === undefined || arr1[i1] <= arr2[i2]) {
+			newArr.push(arr1[i1++])
+		} else if (arr1[i1] === undefined || arr2[i2] < arr1[i1]) {
+			newArr.push(arr2[i2++]);
 		} else {
 			break;
 		}
@@ -36,4 +34,4 @@ var my_array = [3,4,6,10,11,15,21, 22];
 var another_array = [1,5,8,12,14,19];
 console.log(mergeArrays(my_array, another_array)); // -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
 
-// module.exports = mergeArrays;
+module.exports = mergeArrays;
