f120c4b60a902957cc4d205f56aa72696fec32f4
tipdub <mstephenlin@gmail.com>
2017-12-02 09:26:56 -0800

    finished

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,8 +13,15 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
 
+// merge first with concat?
+// then sort?
+
+function mergeArrays(arr1, arr2) {
+	let arr3 = arr1.concat(arr2);
+	return arr3.sort(function(a,b){
+		return a-b;
+	});
 }
 
 module.exports = mergeArrays;
