a52b24a5972bb829cf70a47b715ecbd21b4c2113
ryanlondon <ryel590@gmail.com>
2017-12-04 10:03:04 -0800

    progress

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,43 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
-
+function groupByPlaces(arr) {
+	const groupedArr = [];
+	for (let i = arr.length - 1; i >= 0; i -= 3) {
+		const group = [];
+		if (arr[i-2]) group.push(arr[i-2]);
+		if (arr[i-1]) group.push(arr[i-1]);
+		if (arr[i]) group.push(arr[i]);
+		
+		groupedArr.unshift(group);
+	}
+	return groupedArr;
 }
+
+function numToWords(num) {
+	let words = '';
+	const digitText = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+	const placesText = ['', 'Thousand', 'Million', 'Billion', 'Trillion', 'Quadrillion'];
+  const digitArr = num.toString().split('');
+  const groupedArr = groupByPlaces(digitArr);
+	
+	groupedArr.forEach((group, i) => {
+		let groupStr = '';
+		if (group.length === 3) {
+			groupStr += group[0] + 'Hundred' ? digitText[group[0]] : '';
+			groupStr += group[1] + 'teen' ? digitText[group[1]] : '';
+			groupStr += group[2] ? digitText[group[3]] : '';
+		} else if (group.length === 2) {
+			groupStr += group[0] + 'teen' ? digitText[group[0]] : '';
+			groupStr += group[1] ? digitText[group[1]] : '';
+		} else {
+			groupStr += group[0] ? digitText[group[0]] : '';
+		}
+		words += groupedArr.lengthlength - i ? groupStr + groupedArr[groupedArr.lengthlength - i] : '';
+	});
+	return words;
+}
+
+console.log(numToWords(2000));
 
 module.exports = numToWords;
