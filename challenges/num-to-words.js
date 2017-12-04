10bda80807df19011ae554bb73864ab7217bcc5e
tipdub <mstephenlin@gmail.com>
2017-12-04 10:00:29 -0800

    ???

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,61 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+//turn number into string
+//break into chunks of thousands, or three digit places (000)
+//convert each chunk to string (each chunk is less than 999)
+//add scale to each of them
+//check place value to know what to append to the end
+//
+//only if it's 11-19 is it different
 
+function numToWords(num) {
+	let oneToNineteen = ['', 'one', 'two', 'three', 'four','five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'];
+	let placeValue = [];
+
+	while (num > 0){
+		placeValue.push(number % 1000);
+		num = Math.floor(num/1000);
+	}
+
+	return placeValue; 
+
+	function toString(num){
+		let thousandsString, hundredsString, tensString, onesString, wordsString = [];
+
+		if (number < 20){
+			return oneToNineteen[number-1];
+		}
+
+		if (number < 100){
+			ones = number % 10;
+			tens = Math.floor(number/10);
+			words.push(tens[tensString-1]);
+			words.push(toString(onesString));
+
+			return wordsString.filter(isTruthy).join("-")''
+		}
+
+		hundredsString = number / 100 | 0;
+		wordsString.push(toString(hundredsString));
+		wordsString.push("hundred");
+		wordsString.push(toString(number % 100));
+
+		return wordsString.filter(isTruthy).join(" ");
+	}
+
+	function addScale(chunk, exp){
+		let scale;
+		if(!chunk){
+			return null;
+		}
+		scale = scales[exp-1];
+		return [chunk,scale].filter(isTruthy).join(" ");
+	}
+
+	let string = chunk(number).map(inEnglish).map(appendScale).filter(isTruthy).reverse().join(" ");
 }
 
 module.exports = numToWords;
