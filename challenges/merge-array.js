03a234253bce0d4e17d0fe028012771ac86cebc5
Ringo Yip <ringoyip0901@gmail.com>
2017-12-04 10:01:56 -0800

    interesting hackhour

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -15,6 +15,25 @@
 
 function mergeArrays(arr1, arr2) {
 
+//you have two arrays arr1, arr2;
+//push item arr1 then arr2 and rotate
+//if the number of arr1 is greater than that of arr2, we will add the arr2 item to result. if not, then add item from arr1? but every time we have to splice it?
+	const result = [];
+	while (arr1.length || arr2.length) {
+		if (arr1[0] === undefined) { //this is when arr1 has finally no more elements. 
+			result.push(arr2[0]);
+			arr2.splice(0,1); //eliminate the number that has been added to the result array already
+		}
+		else if (arr1[0] > arr2[0]) { //if the number from arr1 is greater than that of arr2, then we want to add the number from arr2 first because it is smaller. 
+			result.push(arr2[0]);
+			arr2.splice(0,1);
+		}
+		else {
+			result.push(arr1[0]); //same as the previous condtion except it's for the other array
+			arr1.splice(0,1)
+		}
+	}
+	return result;
 }
 
 module.exports = mergeArrays;
---
+++ challenges/num-to-words.js
@@ -0,0 +1,63 @@
+// Write a function that takes a number as an argument and returns its english word representation as a string. Answers should be in upper camel case (a.k.a. Pascal case). Don't worry about spaces.
+// Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
+
+
+/**
+ * numToWords(0) -> 'Zero'
+ * numToWords(43) -> 'FortyThree'
+ * numToWords(2999) -> 'TwoThousandNineHundredNintyNine'
+ * numToWords(15) -> 'Fifteen'
+ * numToWords(2483579411) -> 'TwoBillionFourHundredEightyThreeMillionFiveHundredSeventyNineThousandFourHundredEleven'
+ * numToWords(300525151340440) -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
+ * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
+ */
+
+function numToWords(num) {
+  if (num === 0) {return 'zero'};
+  let length = num.toString().split("").length;
+  let arr = num.toString().split("");
+	const singleDigits = ["", 'One','Two','Three','Four','Five','Six','Seven','Eight','Nine'];
+	const tenDigits = ['','Eleven','Twelve','Thirteen','Forteen','Fiftee','Sixteen','Seventeen','Eighteen','Ninteen'];
+	const twoDigits = ['', "",'Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety'];
+	
+	const tensPower = ["Hundreds",'Thousands'];
+	
+	if (length === 1) {
+	  let index = arr[0];
+	  return singleDigits[index]
+	}
+	else if (length === 2 && num < 20) {
+	  let index = arr[arr.length-1]
+	  return tenDigits[index]
+	}
+	else if (length ===2 && num < 100) {
+	  let index10 = arr[0];
+	  let index = arr[1];
+	  return twoDigits[index10] + singleDigits[index];
+	}
+	else if (length ===3 && num < 1000) {
+	  let indexH = arr[0];
+	  let indexT = arr[1];
+	  let indexU = arr[2];
+	  if (num%100 === 0) {
+	    return singleDigits[indexH] + tensPower[0]
+	  }
+	  return singleDigits[indexH] + tensPower[0] + 'and'+  twoDigits[indexT] + singleDigits[indexU]
+	}
+	else if (length === 4 && num < 10000) {
+	  let indexTH = arr[0]
+	  let indexH = arr[1];
+	  let indexT = arr[2];
+	  let indexU = arr[3];
+	  if (num%1000 === 0) {
+	    return singleDigits[indexTH] + tensPower[1]
+	  }
+	  else {
+	    return singleDigits[indexTH] + tensPower[1] + singleDigits[indexH] + tensPower[0] + 'and' + twoDigits[indexT] + singleDigits[indexU]
+	  }
+	  
+	}
+}
+
+
+module.exports = numToWords;
