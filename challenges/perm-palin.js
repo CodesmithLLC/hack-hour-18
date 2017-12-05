0d9ec8338e26fff145b94cc8c611b9138f18eca8
tipdub <mstephenlin@gmail.com>
2017-12-05 10:15:51 -0800

    ?

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,37 @@
  *
  */
 
+//instead of checking if any permutation is a palindrome
+//can instead check if even, if the count for each letter is even
+//and if odd, check if the count for each letter is even, and the last letter odd
 function permPalin(str) {
-	
+	let charCount = {};
+	for (let i=0; i < str.length; i++){
+		let letter = str[i];
+		charCount[letter] = (charCount[letter] || 0) + 1;
+	}
+	let charCountLen = Object.keys(charCount).length;
+
+	if (charCountLen % 2 == 
+
 }
+
+	// var permutations = [];
+
+	// if (string.length < 2) return string; // This is our break condition
+
+ //    for (var i=0; i<string.length; i++) {
+ //    	var char = string[i];
+
+ //        // Cause we don't want any duplicates:
+ //        if (string.indexOf(char) != i) // if char was used already
+ //            continue;           // skip it this time
+
+ //        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+
+ //        for (var subPermutation of permut(remainingString))
+ //            permutations.push(char + subPermutation)
+ //    }
+
 
 module.exports = permPalin;
