f78b8f6d07bf969d185588dfd456099b51924c70
LazarusCrown <azragcapra@protonmail.com>
2017-12-05 09:55:23 -0800

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -1,5 +1,6 @@
 /**
- * Given a string, determine if any of the permutations of that string is a palindrome
+ * Given a string, determine if any of the permutations of that 
+ * string is a palindrome
  *
  * Example:
  * 	- permPalin('abab') => true
@@ -10,7 +11,33 @@
  */
 
 function permPalin(str) {
-	
+    let strarray = str.split("");
+    let lettercollection = { };
+
+    for(let i=0;i<strarray.length;i++){
+      if(!lettercollection[strarray[i]]){
+        lettercollection[strarray[i]] = 1;
+      }else{
+        lettercollection[strarray[i]]++;
+      }
+    }
+
+    let exception = 0;
+
+    for(var key in lettercollection){
+        if(lettercollection[key]===1){
+            exception++;
+        }
+    }
+
+    if(exception>1){
+        return false;
+    }else{
+        return true;
+    }
 }
 
 module.exports = permPalin;
+
+
+// strarray = ['a','b','c']
