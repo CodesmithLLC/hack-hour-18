aa4d373eec6973d71b60553e4e17ffacf01f9456
Henry <henryau01@gmail.com>
2017-12-05 09:32:09 -0800

    Finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,37 @@
  * 	- permPalin('a') => true
  *
  */
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
+
+function getPermutations(str) {
+  if (str.length < 2) return str;
+  const permutations = [];
+  for (let i = 0; i < str.length; i++) {
+    const curr = str[i];
+
+    if (str.indexOf(curr) !== i) continue;
+
+    const remainingStr = str.slice(0, i) + str.slice(i + 1);
+    for (let subPermutation of getPermutations(remainingStr)) {
+      permutations.push(curr + subPermutation);
+    }
+  }
+  return permutations;
+}
 
 function permPalin(str) {
-	
+  const permArr = getPermutations(str);
+  for (let i = 0; i < permArr.length; i++) {
+    if (isPalindrome(permArr[i])) return true;
+  }
+  return false;
 }
+
+// console.log(permPalin('abab'));
+// console.log(permPalin('cbaba'));
+// console.log(permPalin('cbac'));
+// console.log(permPalin('a'));
 
 module.exports = permPalin;
