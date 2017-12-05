7b242d63b356aae41aae640d403149d586ba5601
justin.ko43 <justin.ko43@gmail.com>
2017-12-05 09:31:08 -0800

    finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,20 @@
  */
 
 function permPalin(str) {
-	
+    console.log('inputs: ', str);
+    let string = str.toLowerCase();
+    let storage = {};
+    for (let i = 0; i < str.length; i++){
+        if (storage[str[i]]) storage[str[i]]++;
+        else storage[str[i]] = 1;
+    }
+    let array = Object.values(storage);
+    let counter = 0;
+    return array.reduce((acc, curval) => {
+        if (counter === 1) return false && acc;
+        if (curval === 1) counter++;
+        return acc && true;
+    }, true)
 }
 
 module.exports = permPalin;
