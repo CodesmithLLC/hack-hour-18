dd701595ab20a95432bbdd2a66e7e487f71ea423
Kevin Gabinete <kgabinete08@gmail.com>
2017-12-05 10:00:33 -0800

    unfinished solutions

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,88 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
-
+const ones = {
+  '0': 'Zero',
+  '1': 'One',
+  '2': 'Two',
+  '3': 'Three',
+  '4': 'Four',
+  '5': 'Five',
+  '6': 'Six',
+  '7': 'Seven',
+  '8': 'Eight',
+  '9': 'Nine',
 }
+
+const teens = {
+  '11': 'Eleven',
+  '12': 'Twelve',
+  '13': 'Thirteen',
+  '14': 'Fourteen',
+  '15': 'Fifteen',
+  '16': 'Sixteen',
+  '17': 'Seventeen',
+  '18': 'Eighteen',
+  '19': 'Nineteen',
+}
+
+const tens = {
+  '1': 'Ten',
+  '2': 'Twenty',
+  '3': 'Thirty',
+  '4': 'Forty',
+  '5': 'Fifty',
+  '6': 'Sixty',
+  '7': 'Seventy',
+  '8': 'Eighty',
+  '9': 'Ninety',
+};
+
+function numToWords(num) {
+  if (typeof num !== 'number') return;
+  let newStr = '';
+  let numStr = num.toString().split('');
+  let length = numStr.length;
+  let temp;
+
+  if (num > 10 && num < 20) {
+    const teen = num.toString();
+    newStr += `${teens[teen]}`
+    return newStr;
+  }
+
+  if (length === 5) {
+    newStr += `${tens[numStr[0]]}`;
+    numStr.shift();
+    length -= 1
+  }
+  if (length === 4) {
+    newStr += `${ones[numStr[0]]}Thousand`;
+    numStr.shift();
+    length -= 1
+  }
+  if (length === 3) {
+    newStr += `${ones[numStr[0]]}Hundred`;
+    numStr.shift();
+    length -= 1
+  }
+  if (length === 2) {
+    newStr += `${tens[numStr[0]]}`;
+    numStr.shift();
+    length -= 1
+  }
+  if (length = 1) {
+    newStr += `${ones[numStr[0]]}`;
+  }
+  return newStr;
+}
+
+console.log(numToWords(0))// -> 'Zero'
+console.log(numToWords(43))// -> 'FortyThree'
+console.log(numToWords(2999))// -> 'TwoThousandNineHundredNintyNine'
+console.log(numToWords(15))// -> 'Fifteen'
+console.log(numToWords(2483579411))// -> 'TwoBillionFourHundredEightyThreeMillionFiveHundredSeventyNineThousandFourHundredEleven'
+console.log(numToWords(300525151340440))// -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
+console.log(numToWords(92120000000000000))// -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
 
 module.exports = numToWords;
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,30 @@
  *
  */
 
+function getPerms(string) {
+  if (string.length < 2) return string;
+  const perms = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    let remainingString = `${string.slice(0 , i)}${string.slice(i + 1,string.length)}`;
+
+    for (let subPerm of getPerms(remainingString))
+      perms.push(char + subPerm)
+  }
+  return perms;
+}
+
 function permPalin(str) {
-	
+  const perms = getPerms(str);
+  let reverseStr;
+
+  for (let i = 0; i < perms.length; i += 1) {
+    reverseStr = perms[i].split('').reverse().join('');
+    console.log(str, reverseStr);
+    if (str === reverseStr) return true;
+  }
 }
 
 module.exports = permPalin;
