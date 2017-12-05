a3367fe2cce66073608c16b81bd16a7dca6c328c
Aaron Elkins <aaronelkins@Aarons-MacBook-Pro.local>
2017-12-05 09:35:20 -0800

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,20 @@
  */
 
 function permPalin(str) {
-	
+  if(typeof str !== 'string' || str.length === 0) return;
+  for(let i = 0;i < str.length;i++){
+    if(str.split("").reverse().join("") === str) {
+      return true;
+    } else if (str[i] === str[i+2]) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log(permPalin('abab'));
+console.log(permPalin('cbaba'));
+console.log(permPalin('cbac'));
+console.log(permPalin('a'));
 
 module.exports = permPalin;
