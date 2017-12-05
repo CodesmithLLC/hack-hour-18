d0cb6e2217829565b829a5e32e9e74a217e01f82
Jonathan Lee <H@Jonathans-MacBook-Pro.local>
2017-12-05 09:28:09 -0800

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -3,14 +3,18 @@
  *
  * Example:
  * 	- permPalin('abab') => true
- * 	- permPalin('cbaba') => true
+ * 	- permPalin('cbaba') => true ababc
  * 	- permPalin('cbac') => false
  * 	- permPalin('a') => true
  *
  */
 
 function permPalin(str) {
-	
+    var re = /[\W_]/g
+    string = str.toLowerCase().replace(re, "")
+    reverseStr = str.split("").reverse().join("")
+
+    return string === reverseStr
 }
 
 module.exports = permPalin;
