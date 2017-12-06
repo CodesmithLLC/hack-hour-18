a605a68cb07f3862aeb670f70c2f8519c2dcd6d6
byanofsky <byanofsky@me.com>
2017-12-05 20:16:09 -0800

    Complete per palin challenge

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,29 @@
  */
 
 function permPalin(str) {
-	
+  if (typeof str !== 'string') throw new Error('Must pass string');
+  const letterCounts = {};
+  for (let i = 0; i < str.length; i += 1) {
+    const curLetter = str[i];
+    if (!(letterCounts[curLetter])) letterCounts[curLetter] = 0;
+    letterCounts[curLetter] += 1;
+  }
+  let seenOdd = false;
+  const counts = Object.values(letterCounts);
+  for (let i = 0; i < counts.length; i += 1) {
+    const count = counts[i];
+    if (count % 2 === 1) {
+      if (seenOdd) return false;
+      seenOdd = true;
+    }
+  }
+  return true;
 }
+
+console.assert(permPalin('abab') === true);
+console.assert(permPalin('cbaba') === true);
+console.assert(permPalin('cbac') === false);
+console.assert(permPalin('a') === true);
+console.assert(permPalin('') === true);
 
 module.exports = permPalin;
