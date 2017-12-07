f55058d6d64c4da487c2b087c49a9cc61fc57b95
Kevin Gabinete <kgabinete08@gmail.com>
2017-12-07 09:56:58 -0800

    stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,16 @@
 
 
 function Stack() {
+  this.body = [];
 
+  this.push = (val) => {
+    this.body.push(val);
+    return this.body.length;
+  }
+
+  this.pop = () => {
+    return this.body.pop();
+  }
 }
 
 
@@ -14,7 +23,27 @@ function Stack() {
 
 
 function Queue() {
+  this.head = new Stack();
+  this.tail = new Stack();
+  this.length = 0;
+  let headIdx = 0;
 
+  this.enqueue = (val) => {
+    this.length += 1;
+    this.tail.push(val);
+    this.head.push(this.tail.body[headIdx]);
+    return this.length;
+  }
+
+  this.dequeue = () => {
+    if (this.length <= 0) return;
+    const item = this.head.pop();
+
+    headIdx += 1;
+    this.length -= 1;
+    this.head.push(this.tail.body[headIdx]);
+    return item;
+  }
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+module.exports = { Stack: Stack, Queue: Queue };
