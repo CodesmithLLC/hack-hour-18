e8eac58801bd9932a8fabcb6b9263e2d8272fd94
ryanlondon <ryel590@gmail.com>
2017-12-08 09:24:54 -0800

    gcd done

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,13 @@
  */
 
 function gcd(a, b) {
-
+  if (b === 0) return a;
+  return gcd(b, (a % b));
 }
+
+// console.log(gcd(10, 8)); // -> 2
+// console.log(gcd(10, 9)); // -> 1
+// console.log(gcd(90, 5)); // -> 5
+// console.log(gcd(333, 111)); // -> 111
 
 module.exports = gcd;
