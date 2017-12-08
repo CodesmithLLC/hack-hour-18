c85ac3cadd825514c47dc34f01f90f547b627275
justin.ko43 <justin.ko43@gmail.com>
2017-12-08 09:24:42 -0800

    finished gcd and stack-queue

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
-
+    function divisor(num){
+        let array = [];
+        for (let i = 1; i <= num; i++){
+            if (num % i === 0) {
+                array.push(i);
+            }
+        }
+        return array;
+    }
+    let arrayA = divisor(a);
+    let arrayB = divisor(b);
+    let commonArr = arrayA.filter((value) => arrayB.indexOf(value) !== -1);
+    return Math.max(...commonArr);
 }
 
 module.exports = gcd;
--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,13 @@
 
 
 function Stack() {
-
+    this.array = [];
+    this.pop = function(){
+        return this.array.pop();
+    }
+    this.push = function(element){
+        this.array.push(element)
+    }
 }
 
 
@@ -14,7 +20,18 @@ function Stack() {
 
 
 function Queue() {
-
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+    this.enqueue = function(element){
+        while (this.stack1.array.length !== 0) this.stack2.push(this.stack1.pop())
+        this.stack1.push(element);
+        while (this.stack2.array.length !== 0) this.stack1.push(this.stack2.pop())        
+    }
+    this.dequeue = function(element){
+        if (this.stack1.array.length === 0) return undefined;
+        return this.stack1.pop();
+    }
 }
+
 
 module.exports = {Stack: Stack, Queue: Queue};
