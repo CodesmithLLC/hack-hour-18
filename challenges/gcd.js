ef6868b394c27071633ad4357c6276dcf241a95c
Tiffany Lin <tiffanylin@Andrew-Chan.local>
2017-12-08 09:43:48 -0800

    gcd complete

---
+++ challenges/gcd.js
@@ -0,0 +1,21 @@
+/**
+ * Given two integers, return the greatest common divisor.
+ *
+ * Example:
+ * gcd(10, 8) -> 2
+ * gcd(10, 9) -> 1
+ *
+ */
+
+function gcd(a, b) {
+  let ints = [a, b].sort(function(a, b) {
+    return a - b;
+  });
+  for (let i = ints[1]; i > 0; i--) {
+    if (ints[0] % i === 0 && ints[1] % i === 0) {
+      return i;
+    }
+  }
+}
+
+module.exports = gcd;
