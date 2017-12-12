6a2721f01a710f1359f3a51d085051b5e03990a5
Henry Au <henryau01@gmail.com>
2017-12-12 09:48:54 -0800

    Finished count-twos (brute force)

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -6,9 +6,22 @@
 // countTwos(1000);  -> 300
 // countTwos(11420);  -> 4483
 
+// 2 12 22 32 42 52 62 72 82 92 20-29 : 10 + 10
 
+// 0 - 100 : 10 + 10 = 20
+// 200 - 299 : 100 + 120 = 120
+// 0 100 200 300 400 500 600 700 800 900
+// 0 - 1000 : 20 * 9 + 120
+
+// BRUTE FORCE
 function countTwos(num) {
-
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    count += (String(i).match(/2/gi) || []).length;
+  }
+  return count;
 }
+
+// console.log(countTwos(1000));
 
 module.exports = countTwos;
