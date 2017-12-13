5ecc14b8eae682376f5c8369e27eb45a167b1869
Kevin Gabinete <kgabinete08@gmail.com>
2017-12-13 09:56:33 -0800

    add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,26 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let curr1 = l1;
+  let curr2 = l2;
+  let carry;
 
+  while (curr1 && curr2) {
+    if (carry) {
+      curr1.value += 1;
+      carry = false;
+    }
+    curr1.value += curr2.value;
+    if (curr1.value >= 10) {
+      curr1.value -= 10;
+      carry = true;
+    }
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+  }
+  return l1;
 }
+
+console.log(addLinkedList(node1a, node1b));
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
