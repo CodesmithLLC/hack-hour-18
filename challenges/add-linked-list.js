fbb101a2286cfd82ab7771c855734d4d916375c9
Henry <henryau01@gmail.com>
2017-12-13 12:15:48 -0800

    Gimme green cheks

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,53 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let curr1 = l1;
+  let curr2 = l2;
+  let l3;
+  let result;
+  let carry = 0;
+  while (curr1 && curr2) {
+    let val = curr1.value + curr2.value + carry;
+    carry = 0;
+    if (val > 9) {
+      carry = Math.floor(val / 10);
+      val %= 10;
+    }
+    if (!result) {
+      result = new Node(val);
+      l3 = result;
+    } else {
+      const node = new Node(val);
+      l3.next = node;
+      l3 = l3.next;
+    }
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+  }
+  if (curr1) l3.next = curr1;
+  if (curr2) l3.next = curr2;
+  if (carry && l3.next) {
+    const val = l3.next.value + carry;
+    const save = l3.next.next;
+    l3.next = new Node(val);
+    l3.next.next = save;
+  } else if (carry) l3.next = new Node(carry);
 
+  return result;
 }
+
+// 1234 + 18 = 204
+// 12344 + 183 = 20744
+const l1 = new Node(1);
+l1.next = new Node(2);
+l1.next.next = new Node(3);
+l1.next.next.next = new Node(4);
+l1.next.next.next.next = new Node(4);
+const l2 = new Node(1);
+l2.next = new Node(8);
+l2.next.next = new Node(3);
+l1
+l2
+console.log(JSON.stringify(addLinkedList(l1, l2), null, 2));
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
