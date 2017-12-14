c6d5af20735948a9b0a349e7885697e517b52ac0
jiminykbob <jikang92@hotmail.com>
2017-12-14 11:33:59 -0800

    finish superbalanced

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -7,14 +7,40 @@
 example: http://www.geeksforgeeks.org/wp-content/uploads/balanced_tree.GIF
  */
 
+ // A tree is "superbalanced" if the difference between
+ // the depths of any two leaf nodes ↴ is no greater than one.
+
 function BinaryTree(value) {
   this.value = value;
   this.left = null;
   this.right = null;
 }
 
-function superbalanced(tree) {
-
+function superbalanced(tree, depth = 0, leafCache = []) {
+  // console.log(tree.value, depth);
+  if (tree.left && tree.right) {
+    return superbalanced(tree.left, depth + 1, leafCache) && superbalanced(tree.right, depth + 1, leafCache)
+  } else if (tree.left) {
+    return superbalanced(tree.left, depth + 1, leafCache)
+  } else if (tree.right) {
+    return superbalanced(tree.right, depth + 1, leafCache)
+  } else {
+    leafCache.push(depth)
+    return leafCache.every(num => Math.abs(num - depth) <= 1)
+  }
 }
+
+// const myTree = new BinaryTree(10);
+// myTree.left = new BinaryTree(5);
+// myTree.left.left = new BinaryTree(3);
+// myTree.left.right = new BinaryTree(7);
+// myTree.left.right.left = new BinaryTree(6);
+// // myTree.left.right.left.right = new BinaryTree(8);
+//
+// myTree.right = new BinaryTree(15);
+// // myTree.right.left = new BinaryTree(12);
+// myTree.right.right = new BinaryTree(20);
+
+// console.log(superbalanced(myTree))
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
