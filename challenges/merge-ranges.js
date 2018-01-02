034e39ad5b3a91f268b52bc36cccaa5cc7e88158
ryanlondon <ryel590@gmail.com>
2018-01-02 14:54:15 -0800

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,21 @@
 
 
 function mergeRanges(array) {
-
+ return array
+  .sort((a, b) => a[0] - b[0])
+  .reduce((ranges, meeting) => {
+    if (!ranges.length) ranges.push(meeting);
+    if (meeting[0] <= ranges[ranges.length - 1][1]) {
+      ranges[ranges.length - 1][1] = meeting[1];
+    } else {
+      ranges.push(meeting);
+    }
+    return ranges;
+  }, []);
 }
+
+
+// var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+// console.log(mergeRanges(times)); // -> [[0, 1], [3, 8], [9, 12]]
 
 module.exports = mergeRanges;
