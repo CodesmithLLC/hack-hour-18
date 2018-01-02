64e483f121620ecb43bd4753ed50735ee95469d7
justin.ko43 <justin.ko43@gmail.com>
2018-01-02 10:42:01 -0800

    finished merge range

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -1,5 +1,6 @@
 /**
- * Write a function mergeRanges that takes an array of meeting time ranges and returns an array of condensed ranges.
+ * Write a function mergeRanges that takes an array of meeting time ranges 
+ * and returns an array of condensed ranges.
  *
  * Example:
  * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
@@ -11,7 +12,15 @@
 
 
 function mergeRanges(array) {
-
-}
+  let newArr = array.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  let result = [], last;
+  newArr.forEach(ele => {
+    if (!last || ele[0] > last[1]) result.push(last = ele);
+    else if (ele[1] > last[1]) last[1] = ele[1];
+  });
+  return result;
+ }
 
 module.exports = mergeRanges;
