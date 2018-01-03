8ac2a96412c5cbbae8c9a7ccc1d701a2b2279440
Henry <henryau01@gmail.com>
2018-01-03 09:35:34 -0800

    Finished linked-list-cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -1,39 +1,56 @@
 /**
- * We are familar with linked lists being linear and terminating:
+ We are familar with linked lists being linear and terminating:
  *
- * A->B->C->D
+ A->B->C->D
  *
- * However, linked lists can also have cyclical references:
+ However, linked lists can also have cyclical references:
  *
- * A->B->C->D
- *    ^     |
- *    |     V
- *    G<-F<-E
+ A->B->C->D
+    ^     |
+    |     V
+    G<-F<-E
  *
- * Create a function that accepts a linked list and returns true if the linked list has a cyclical reference
+ Create a function that accepts a linked list and returns true if the linked list has a cyclical reference
  *
- * var node1 = new Node('1');
- * var node2 = node1.next = new Node('2');
- * var node3 = node2.next = new Node('3');
- * var node4 = node3.next = new Node('4');
- * var node5 = node4.next = new Node('5');
- * hasCycle(node1); // => false
- * node5.next = node2;
- * hasCycle(node1); // => true
+ let node1 = new Node('1');
+ let node2 = node1.next = new Node('2');
+ let node3 = node2.next = new Node('3');
+ let node4 = node3.next = new Node('4');
+ let node5 = node4.next = new Node('5');
+ hasCycle(node1); // => false
+ node5.next = node2;
+ hasCycle(node1); // => true
  *
- * Challenge 1: Do this in linear time
- * Challenge 2: Do this in constant space
- * Challenge 3: Do not mutate the original nodes in any way
+ Challenge 1: Do this in linear time
+ Challenge 2: Do this in constant space
+ Challenge 3: Do not mutate the original nodes in any way
  *
  */
 
-var Node = function(value) {
+let Node = function(value) {
   this.value = value;
   this.next = null;
 }
 
 function hasCycle(head) {
-
+  let current = head;
+  let faster = head;
+  while (current || faster) {
+    current = current.next;
+    if (faster.next) faster = faster.next.next;
+    else break;
+    if (current === faster) return true;
+  }
+  return false;
 }
+
+let node1 = new Node('1');
+let node2 = node1.next = new Node('2');
+let node3 = node2.next = new Node('3');
+let node4 = node3.next = new Node('4');
+let node5 = node4.next = new Node('5');
+console.log(hasCycle(node1)); // => false
+node5.next = node2;
+console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
