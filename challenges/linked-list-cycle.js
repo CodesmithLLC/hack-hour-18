172a6518c7e67a4c42f6aead0e1cca682b6075df
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2018-01-03 09:40:27 -0800

    finished linked list challenge

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,27 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  const storage = new Set();
+  let currentNode = head;
+  while (currentNode) {
+    if (storage.has(currentNode)) {
+      return true;
+    }
+    else {
+      storage.add(currentNode);
+      currentNode = currentNode.next;
+    }
+  }
+  return false;
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
