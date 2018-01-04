e2c1e6e1dd4c41fd65f6d2a3b888c3fcd09d4737
Henry <henryau01@gmail.com>
2018-01-04 09:38:07 -0800

    Finished multiples of 3 or 5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -6,19 +7,47 @@
 // below 1000 and return that sum.
 
 function sumMultiples3Or5Below1000() {
+  const arr = [];
   let sum = 0;
-
+  for (let i = 1; i * 3 < 10 || i * 5 < 10; i++) {
+    const threes = i * 3;
+    const fives = i * 5;
+    if (threes < 10 && !arr.includes(threes)) {
+      arr.push(threes);
+      sum += threes;
+    }
+    if (fives < 10 && !arr.includes(fives)) {
+      arr.push(fives);
+      sum += fives;
+    }
+  }
   return sum;
 }
+
+// console.log(sumMultiples3Or5Below1000());
 
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
+  const arr = [];
   let sum = 0;
-
+  for (let i = 1; i * x < z || i * y < z; i++) {
+    const xs = i * x;
+    const ys = i * y;
+    if (xs < z && !arr.includes(xs)) {
+      arr.push(xs);
+      sum += xs;
+    }
+    if (ys < z && !arr.includes(ys)) {
+      arr.push(ys);
+      sum += ys;
+    }
+  }
   return sum;
 }
+
+// console.log(sumMultiplesXOrYBelowZ(3, 5, 10));
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
