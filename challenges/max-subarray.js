fb735c0d3aba91905aa5c0467418294b99c3606d
Henry <henryau01@gmail.com>
2018-01-05 09:17:20 -0800

    Finished max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,18 @@
  */
 
 function maxSubarray(arr) {
-
+  if (!Array.isArray(arr) || arr.length === 0) return 0;
+  let currentMax = arr[0];
+  let maxSum = currentMax;
+  for (let i = 1; i < arr.length; i++) {
+    currentMax += arr[i];
+    maxSum = Math.max(currentMax, maxSum);
+    if (currentMax < 0) currentMax = 0;
+  }
+  return maxSum;
 }
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+// console.log(maxSubarray([15, 20, -5, 10]));
 
 module.exports = maxSubarray;
