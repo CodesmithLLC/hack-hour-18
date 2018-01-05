bcebe1136412afe856cb65f6a0e732891917122a
jiminykbob <jikang92@hotmail.com>
2018-01-05 09:49:17 -0800

    max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,43 @@
  */
 
 function maxSubarray(arr) {
+  let sum = 0;
+  let maxSum = -Infinity;
+  let subarr = [];
+  let maxSubarr = [];
 
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+      subarr.push(arr[i]);
+    }
+    else if (Math.abs(arr[i]) > sum) {
+      if (sum > maxSum) {
+        maxSum = sum;
+        maxSubarr = subarr;
+      }
+      subarr = [];
+      sum = 0;
+    }
+    else if (arr[i + 1] + arr[i] >= 0) {
+      sum += arr[i];
+      subarr.push(arr[i]);
+    }
+    else {
+      if (sum > maxSum) {
+        maxSum = sum;
+        maxSubarr = subarr;
+      }
+      subarr = [];
+      sum = 0;
+    }
+    console.log(subarr)
+  }
+  return maxSubarr;
 }
+
+// const array1 = [1, -2, 3, 10, -4, 7, 2, -5];
+// const array2 = [15,20,-5,10];
+// console.log(maxSubarray(array1));
 
 module.exports = maxSubarray;
