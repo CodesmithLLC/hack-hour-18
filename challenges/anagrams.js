4ce93b45a741e3d816508f7f582abeeed74d65bc
Henry <henryau01@gmail.com>
2018-01-06 09:23:42 -0800

    Finished anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,32 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
-
+function permuteHelper(str, chosen, result) {
+  // console.log(str, chosen, result);
+  if (!str.length) {
+    result.add(chosen.join(''));
+  } else {
+    // choose/explore/unchoose
+    for (let i = 0; i < str.length; i++) {
+      // choose
+      const c = str[i];
+      chosen.push(c);
+      str.splice(i, 1);
+      // explore
+      permuteHelper(str, chosen, result);
+      // unchoose
+      str.splice(i, 0, c);
+      chosen.pop();
+    }
+  }
 }
+
+function anagrams(str) {
+  const result = new Set();
+  permuteHelper(str.split(''), [], result);
+  return [...result];
+}
+
+// console.log(anagrams('aba'));
 
 module.exports = anagrams;
