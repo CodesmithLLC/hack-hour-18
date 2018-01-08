c3e8523b776e74ec991f1c98f672b8bdf530db6c
eric <ericcodesweb@gmail.com>
2018-01-08 09:38:21 -0800

    delete dups again

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,27 @@
   */
 
 function anagrams(string) {
+  if (string === '') return [];
 
+  function helper(str, pos, combos) {
+    let result = [];
+    console.log(combos);
+    if (str === '' || pos === str.length) return combos;
+
+    let cur = str[pos];
+
+    for (let i=0; i < combos.length; i++) {
+      for (let j=0; j < combos[i].length; j++) {
+        console.log('combos', combos[i].split('').slice().splice(i, 0, cur));
+        result.push(combos[i].split('').slice(0, i).concat([cur]).concat(combos[i].split('').slice(i)).splice(i, 0, cur).join(''));
+      }
+    }
+    helper(str, pos + 1, result);
+  }
+  return helper(string, 0, [ string[0] ]);
 }
+
+console.log(anagrams('abc'));
+// console.log('test', ['a'].splice(0, 0, 'b'));
 
 module.exports = anagrams;
