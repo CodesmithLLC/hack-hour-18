260498b327411cc601e15075f1c2468f976145a0
Kevin Gabinete <kgabinete08@gmail.com>
2018-01-08 10:13:30 -0800

    delete dupes linked list

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,21 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
 
 function deleteDups(head) {
+  const array = [];
+  let current = head;
 
+  while (current) {
+    if (array.includes(current.value)) {
+      current.next = current.next.next;
+    }
+    array.push(current.value);
+  }
 }
 
 module.exports = deleteDups;
