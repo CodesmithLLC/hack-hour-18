b8fe9157d0f37fd0a750e93ffc0d027238009241
Henry <henryau01@gmail.com>
2018-01-08 09:37:12 -0800

    Finished delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,31 @@
 
 
 function deleteDups(head) {
-
+  let current = head;
+  const cache = [];
+  while (current) {
+    cache.push(current.value);
+    while (current.next && cache.includes(current.next.value)) {
+      current.next = current.next.next;
+    }
+    current = current.next;
+  }
+  return head;
 }
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+// const node = new Node(1);
+// node.next = new Node(2);
+// node.next.next = new Node(3);
+// node.next.next.next = new Node(3);
+// node.next.next.next.next = new Node(3);
+// node.next.next.next.next.next = new Node(3);
+// console.log(JSON.stringify(node));
+
+// console.log(JSON.stringify(deleteDups(node)));
 
 module.exports = deleteDups;
