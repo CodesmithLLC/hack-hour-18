924f2a9c5c9743e2bf277319638bc540b8b2074c
Tom Rosenblatt <rosenblatttom@gmail.com>
2018-01-09 10:00:29 -0800

    completed delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -14,7 +14,36 @@
 // approach: while loop through linkedList and store each value in object/set.
 // if repeated value, then reassign links in list to remove node
 function deleteDups(head) {
-
+  let current = head;
+  const store = {};
+  let beforeNodeTemp;
+  while (current) {
+    if (current.value in store) {
+      beforeNodeTemp.next = current.next;
+    } else {
+      store[current.value] = 0;
+      beforeNodeTemp = current;
+    }
+    current = current.next;
+  }
 }
 
 module.exports = deleteDups;
+
+
+function LinkedList(value) {
+  this.value = value;
+  this.next = null;
+}
+
+const a = new LinkedList(1);
+const b = new LinkedList(2);
+const c = new LinkedList(3);
+const d = new LinkedList(3);
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+deleteDups(a);
+console.log(JSON.stringify(a));
