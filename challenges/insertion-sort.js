c1434219075af50462c730ca66547601eee3e95a
ryanlondon <ryel590@gmail.com>
2018-01-11 09:59:04 -0800

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j - 1] > array[j]) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
+      j--;
+    }
+  }
+  return array;
 }
+
+const arr = [3,6,5,2,8,7,4,1];
+console.log(insertionSort(arr));
 
 module.exports = insertionSort;
