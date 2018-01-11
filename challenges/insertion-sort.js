311283f96319eeaf683045cfae79ce796271cdc7
Henry <henryau01@gmail.com>
2018-01-11 09:46:49 -0800

    Finished insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,20 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (array[i] < array[j - 1]) {
+      j--;
+    }
+    const hold = array[i];
+    array.splice(i, 1);
+    array.splice(j, 0, hold);
+  }
+  return array;
 }
+
+// const arr = [5, 4, 3, 2, 1];
+// console.log(insertionSort(arr));
 
 module.exports = insertionSort;
