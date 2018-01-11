65746db77881c8c527df69afe4ac288a1667b589
jiminykbob <jikang92@hotmail.com>
2018-01-11 09:45:32 -0800

    fin insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,14 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    let temp = array[i];
+    for (let j = i - 1; j >= 0 && temp < array[j]; j -= 1) {
+      array[j + 1] = array[j];
+      array[j] = temp;
+    }
+  }
+  return array;
 }
-
+// insertionSort([6,5,3,1,8,7,2,4]);
 module.exports = insertionSort;
