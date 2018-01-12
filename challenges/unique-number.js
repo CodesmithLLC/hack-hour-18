37d2584e7032eac489a1dcc8210f90196be2a3a3
mikeg <mikeg@steelhouse.com>
2018-01-12 09:50:33 -0800

    unique number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,23 @@
  *
  */
 function uniqueNumber(array) {
-
+  const hash = {};
+  for (let i = 0; i < array.length; i += 1) {
+    if (hash[array[i]]) {
+      hash[array[i]] += 1
+    } else {
+      hash[array[i]] = 1;
+    }
+  }
+  for (const key in hash) {
+    if (hash[key] === 1) {
+      return key;
+    }
+  }
+  return false;
 }
+
+console.log(uniqueNumber([1, 2, 1, 3, 3]));  // 2
+console.log(uniqueNumber([1, 2, 1, 3, 3, 2, 5, 7, 3, 7]));  // 5
 
 module.exports = uniqueNumber;
