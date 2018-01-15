2a02c45ec4cfa9f0e4b7085c707bd92d41cd15f7
Kevin Gabinete <kgabinete08@gmail.com>
2018-01-15 10:35:02 -0800

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -8,7 +8,7 @@ function LinkedList() {
 }
 
 function Node(val) {
-  this.val = val;
+  this.value = val;
   this.next = null;
   this.prev = null;
 }
@@ -17,14 +17,43 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const node = new Node(val);
+
+  if (!this.head) {
+    this.head = this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let current = this.head;
+
+  if (current.value === val) {
+    this.head = current.next;
+    if (!this.head) {
+      this.tail = null;
+    } else {
+      this.head.prev = null;
+    }
+  } else if (this.tail.value = val) {
+    current = this.tail;
+    this.tail = current.prev;
+    this.tail.next = null;
+  } else {
+    while (current) {
+      if (current.value = val) {
+        current.prev.next = current.next;
+        return;
+      }
+      current = current.next;
+    }
+  }
 };
 
 module.exports = LinkedList;
