bcbe9c06107d19495e0d120787816340af28b7cd
Peter Lee <peter.hbyul.lee@gmail.com>
2018-01-15 09:50:16 -0800

    (complete) doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -14,17 +14,32 @@ function Node(val) {
 }
 
 /*
-Adds a node to the end of the list
- */
 LinkedList.prototype.add = function(val) {
-  
+  if (!this.head) {
+    this.head = new Node(val)
+    this.tail = this.head
+    return;
+  }
+  this.tail.next = new Node(val)
+  this.tail.next.prev = this.tail
+  this.tail = this.tail.next
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (!this.head) return;
+  let temp = this.head
+  while (temp.next) {
+    if (temp.val === val) {
+      temp.prev.next = temp.next
+      temp.next.prev = temp.prev
+      break;
+    }
+    temp = temp.next
+  }
+  if (!temp.next) temp.prev.next = null
 };
 
 module.exports = LinkedList;
