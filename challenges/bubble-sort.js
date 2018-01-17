b4f3c93a0b0fbc887da29cd469978cdd726a3ed4
programminPete <peter.fasula@gmail.com>
2018-01-17 09:31:11 -0800

    bubble-sort attempt 1

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+  if(!Array.isArray(array)) return undefined;
+  let changedFlag = false;
+  for(let i = 0; i < array.length - 1; i++){
+    if(array[i+1]<array[i]){
+      let temp = array[i+1];
+      array[i+1] = array[i];
+      array[i] = temp;
+      changedFlag = true;
+    }
+  }
+  console.log(array)
+  if(changedFlag === true){
+    bubbleSort(array)
+  }
+  return array;
 }
+
 
 module.exports = bubbleSort;
