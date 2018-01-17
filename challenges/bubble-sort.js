6249e8cd786dcc21c31ee22af6d7944e2a0881f8
ryanlondon <ryel590@gmail.com>
2018-01-17 09:19:20 -0800

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = true
+      }
+    }
+    if (swapped === false) return array;
+  }
 }
+
+// const unsorted = [2,5,6,4,3,7,1,8];
+// console.log(bubbleSort(unsorted));
 
 module.exports = bubbleSort;
