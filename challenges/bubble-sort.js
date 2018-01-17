1ef081200347100a9d15b2d5ef78782f177e439c
Kevin Gabinete <kgabinete08@gmail.com>
2018-01-17 09:17:40 -0800

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
+  let counter = 0;
 
+  while (counter < array.length) {
+    for (let i = 0; i < array.length - counter; i += 1) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = temp;
+      }
+    }
+    counter += 1;
+  }
+  return array;
 }
+
+console.log(bubbleSort([3, 2, 7, 9, 4, 1, 5, 8, 6]));
 
 module.exports = bubbleSort;
