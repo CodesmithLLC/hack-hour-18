5b61d4e8881a4a61cc5b8044a5094619dd4e9ec3
Aaron Elkins <akatoll@hotmail.com>
2018-01-23 09:39:29 -0800

    Pascals triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,34 @@
 */
 
 function pascalTriangle(numRows) {
-
+  
+  let arr = [];
+  if (numRows === 1) {
+      arr[0] = [];
+      arr[0][0] = 1;
+  } else if (numRows === 2) {
+      arr[0] = [];
+      arr[0][0] = 1;
+      arr[1] = [];
+      arr[1][0] = 1;
+      arr[1][1] = 1;
+  } else if (numRows > 2) {
+      arr[0] = [];
+      arr[1] = [];
+      arr[0][0] = 1;
+      arr[1][0] = 1;
+      arr[1][1] = 1;
+      for (i = 2; i < numRows; i++) {
+          arr[i] = [];
+          arr[i][0] = 1;
+          for (j = 1; j < i; j++) {
+              arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+          }
+          arr[i][j] = 1;
+      }
+  }
+  arr.forEach(ele => console.log(ele));
+  return arr;
 }
 
 module.exports = pascalTriangle;
