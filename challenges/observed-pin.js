8365c7206acc95e14359c2972152c056f6e87d1b
Aaron Elkins <akatoll@hotmail.com>
2018-01-26 09:43:54 -0800

    Missing Number

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -43,6 +43,40 @@ expectations = {
 
 
 function getPINs(observed) {
+  let numObj = { 
+    "0": function() { return ["8", "0"]; }, 
+    "1": function() { return ["1", "2", "4"]; }, 
+    "2": function() { return ["1", "2", "3", "5"]; }, 
+    "3": function() { return ["2", "3", "6"]; }, 
+    "4": function() { return ["1", "4", "5", "7"]; }, 
+    "5": function() { return ["2", "4", "5", "6", "8"]; }, 
+    "6": function() { return ["3", "5", "6", "9"]; }, 
+    "7": function() { return ["4", "7", "8"]; }, 
+    "8": function() { return ["5", "7", "8", "9", "0"]; }, 
+    "9": function() { return ["6", "8", "9"]; } 
+  };
+
+  let allComb = observed.split("").map(function(curr) {
+    return numObj[curr]();
+  });
+  
+  let num = [];
+  let buildArr = [];
+  
+  return allComb.reduce((prevArr, currArr) => {
+    
+    buildArr = [];
+    addValue = [];
+    
+    while (prevArr.length > 0) {
+      num = prevArr.pop();
+      currArr.map(function(curr) {
+        buildArr.push(addValue + curr);
+      });
+    }
+    return buildArr.sort();
+  });
+  
 
 }
 
