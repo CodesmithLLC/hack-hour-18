18ad5631682ebb242528822ac118f0ff4a01865b
eric <ericcodesweb@gmail.com>
2018-01-30 09:29:46 -0800

    common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,44 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
-
+function uniques(array) {
+  const result = [];
+  for (let i=0; i<array.length; i++) {
+    if (result.indexOf(array[i]) === -1) {
+      result.push(array[i])
+    }
+  }
+  return result;
 }
 
+function commonElements(array1, array2, array3, array4){
+  const storage = {};
+  const result = [];
+  for (let i=0; i<array1.length; i++) {
+    storage[array1[i]] = 1;
+  }
+
+  for (let i=0; i<array2.length; i++) {
+    if (storage[array2[i]] === 1) {
+      storage[array2[i]] = 2;
+    }
+  }
+  for (let i=0; i<array2.length; i++) {
+    if (storage[array3[i]] === 2) {
+      storage[array3[i]] = 3;
+    }
+  }
+  for (let i=0; i<array2.length; i++) {
+    if (storage[array4[i]] === 3) {
+      result.push(array4[i]);
+    }
+  }
+  if (result.length === 0) return "Nothing in Common!";
+  return uniques(result);
+}
+// var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+// var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+// var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+// var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+// console.log(commonElements(array1, array2, array3, array4));
 module.exports = commonElements;
--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -8,9 +8,17 @@
   solveKnapsack(items, 3); // returns 7 (from items[0] and items[1])
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
+function sackGet(itemArray, option) {
+  return itemArray.reduce((acc, cur) => acc += cur[option], 0);
+}
 
 function solveKnapsack(items, weightAvailable) {
+  // const grid = [];
+  // for (let i=0; i<items.length; i++) {
+  //   grid[i] = new Array(items.length - 1);
+}
+items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+// console.log(sackGet(items, 'value'));
 
-};
 
 module.exports = solveKnapsack;
