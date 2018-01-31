1686e3163945f88f64df1a8037b5f87bdcfe871a
Kevin Gabinete <kgabinete08@gmail.com>
2018-01-31 09:49:22 -0800

    each permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -21,9 +21,26 @@ eachPermutation([1, 2, 3], function(perm) {
 */
 
 function eachPermutation(arr, callback) {
-
+  const perms = getPerms(arr);
+  perms.forEach(elem => callback(elem));
 }
 
+function getPerms(array) {
+  let result = [];
 
+  function permute(arr, perm = []) {
+    if (arr.length === 0) {
+      result.push(perm);
+    } else {
+      for (let i = 0; i < arr.length; i += 1) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), perm.concat(next));
+      }
+    }
+  }
+  permute(array);
+  return result;
+}
 
 module.exports = eachPermutation;
