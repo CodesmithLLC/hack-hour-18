fb6ec9f7b494c7121731aba1a4cad6bebb1ce309
programminPete <peter.fasula@gmail.com>
2018-03-21 10:52:51 -0400

    max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,42 @@
  */
 
 function maxSubarray(arr) {
+  while(arr[arr.length-1] < 0) arr.pop();
+  let currentMax = Number.NEGATIVE_INFINITY;
+  let finalMax = arr[0];
 
+  for(let i = 0; i < arr.length; i++){
+
+    if(arr[i] > currentMax + arr[i]){
+      currentMax = arr[i];
+    }else{
+      currentMax = currentMax + arr[i];
+    }
+    //compare currentMax to finalMax
+    if(currentMax > finalMax){
+      finalMax = currentMax;
+    }
+  }
+  console.log(finalMax);
+  return finalMax;
 }
 
+maxSubarray([1, 1, -2, 3, 10, -4, 7, 2, -5])
+maxSubarray([15,20,-5,10])
 module.exports = maxSubarray;
+
+
+/* CODESMITH SOLUTION */
+
+// function maxSubarray(arr) {
+
+//   let currentMax = Number.NEGATIVE_INFINITY;
+//   let finalMax = Number.NEGATIVE_INFINITY;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     currentMax = Math.max(arr[i], currentMax + arr[i]);
+//     finalMax = Math.max(finalMax, currentMax);
+//   }
+//   console.log(finalMax);
+//   return finalMax;
+// }
